9d02af28bc9f848532881b291cbb40396244aebf
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-05-19 10:31:50 +0530

    Update url_helper.js
    URL_ helper page  Remove unnecessary or duplicate  URL variable

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -20,50 +20,29 @@ export const EDIT_ORDER_ID ="/Order/GetItemListByDivisionID?DivisionID=3"
 //Python Logic 
 export const POST_JWT_LOGIN = "/Login"
 
-//ADMINISTRATOR   **** Modules
-// Moduls Submit  Method URL
-export const SUBMIT_H_MODULES ="/H_Modules"
-// Fetch Module List
-export const FETCH_MODULES_LIST ="/H_Modules"
+//ADMINISTRATOR   **** Modules  Moduls Submit  Method URL
+export const H_MODULES_API_URL ="/H_Modules"
 
-//ADMINISTRATOR *** Company 
-// Company List URL
-export const COMPANY_lIST ="/C_Companies"
+//ADMINISTRATOR ***  Company List URL
+export const COMPANY_API_URL ="/C_Companies"
 
-//PAGE Master
-export const GET_DEFAULT_MODULE="/HModules"
-export const POST_MODULE_ID="/HModules"
-
-export const POST_SUB_MODULE="/HSubModules"
-export const POST_ADD_PAGE="/HPages"
 export const GET_PAGE_ACCESS="/HPageAccessNames"
 
-export const H_SUB_MODULES="/H_SubModules"/// post api
+export const H_SUB_MODULES_API_URL="/H_SubModules"/// post api
 
 export const H_SUB_MODULES_FROM_HPAGES="/GetSubModuleFromModuleID"/// post api
-export const HPAGES_LIST="/H_Pages"/// post api
 
-export const GET_EMPLOYEE="/M_Employees"   //// get M_Employees api
-export const GET_ROLE="/M_Roles"///get api
+export const HPAGES_API_URL="/H_Pages"/// post api
+
+export const ROLE_API_URL="/M_Roles"///get api
 
 /// User Registration
-export const GET_EMPLOYEE_API="/M_Employees"   //// get M_Employees api
-export const GET_ROLE_API="/M_Roles"///get api
-export const POST_ADD_USER="/UserList"
-export const USER_GET_API="/UserList"
-export const DELETE_USER="/UserList"
-export const EDIT_USER="/UserList"
-export const UPDATE_SUCCESS_DATA="/UserList"
 
+export const USER_MODULES_API_URL="/UserList"
 
-/// M_Employees api
+// M_Employees api
 export const GET_DESIGNATIONID="/Designation"
 export const GET_EMPLOYEETYPE="/EmployeeTypeByID"
 export const GET_STATE="/State"
 export const GET_REGION="/Region"
-export const POST_EMPLOYEEDATA="/M_Employees"
-export const GET_COMAPNY_API="/ C_Companies"
-export const GET_EMPLOYEE_LIST_API="/M_Employees"
-export const Delete_EmployeeID="/M_Employees"
-export const edit_EmployeeID="/M_Employees"
-export const Update_Employee_ID="/M_Employees"
+export const EMPLOYEE_API_URL="/M_Employees"   
