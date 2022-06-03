429072899f5205b36afdbfaae29aa3d1901990db
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-06-03 11:26:11 +0530

    Update ValidationTest.js

--- src/pages/Purchase/Orders/ValidationTest.js
+++ src/pages/Purchase/Orders/ValidationTest.js
@@ -14,62 +14,73 @@ export default function ValidationTest() {
 
 
     const [validation, setValidation] = useState({
-        fnm: null,
-        lnm: null,
-        unm: null,
-        city: null,
-        stateV: null,
+        valInp1: null,
+        valInp2: null,
+        valInp3: null,
+
     })
 
     function handleSubmit(e) {
-
         e.preventDefault()
         const modifiedV = { ...validation }
-        var fnm = document.getElementById("validationTooltip01").value
-        var lnm = document.getElementById("validationTooltip02").value
-        var unm = document.getElementById("validationTooltip03").value
-        // var city = document.getElementById("validationTooltip03").value
-        // var stateV = document.getElementById("validationTooltip04").value
+        var fnm = document.getElementById("valInp1")
+        var lnm = document.getElementById("valInp2")
+        var unm = document.getElementById("valInp3")
 
-        if (fnm === "") {
-            modifiedV["fnm"] = false
-        } else {
-            modifiedV["fnm"] = true
+        if (fnm.id === "valInp1") {
+            if ((fnm.value !== "")) {
+                modifiedV[fnm.id] = true
+            } else {
+                modifiedV[fnm.id] = false
+            }
         }
 
-        if (lnm === "") {
-            modifiedV["lnm"] = false
-        } else {
-            modifiedV["lnm"] = true
+        if (lnm.id === "valInp2") {
+            if (/^[0-9]+$/.test(lnm.value)) { modifiedV[lnm.id] = true }
+            else {
+                modifiedV[lnm.id] = false
+            }
         }
 
-        if (unm === "") {
-            modifiedV["unm"] = false
-        } else {
-            modifiedV["unm"] = true
+        if (unm.id === "valInp3") {
+            var Emailvalidation = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+            if ((Emailvalidation.test(unm.value))) {
+                modifiedV[unm.id] = true
+            }
+            else {
+                modifiedV[unm.id] = false
+            }
         }
-
-        // if (city === "") {
-        //     modifiedV["city"] = false
-        // } else {
-        //     modifiedV["city"] = true
-        // }
-
-        // if (stateV === "") {
-        //     modifiedV["stateV"] = false
-        // } else {
-        //     modifiedV["stateV"] = true
-        // }
         setValidation(modifiedV)
+
+        if ((modifiedV["valInp1"]) && (modifiedV["valInp2"]) && (modifiedV["valInp3"])) {
+            alert("submit scuccess")
+        }
     }
 
     //for change tooltip display propery
-    const onChangeValidation = (fieldName, value) => {
+    const onChangeValidation = (fieldName, value, catagory) => {
 
         const modifiedV = { ...validation }
-        if (value !== "") {
+        if ((value !== "") && (catagory === "text")) {
             modifiedV[fieldName] = true
-        } else {
+        }
+        else if (catagory === "Number") {
+            if (/^[0-9]+$/.test(value)) { modifiedV[fieldName] = true }
+            else {
+                modifiedV[fieldName] = false
+            }
+        }
+        else if (catagory === "email") {
+            var Emailvalidation = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+            if ((Emailvalidation.test(value))) {
+                modifiedV[fieldName] = true
+            }
+            else {
+                modifiedV[fieldName] = false
+            }
+        }
+        else {
             modifiedV[fieldName] = false
         }
         setValidation(modifiedV)
@@ -80,16 +91,20 @@ export default function ValidationTest() {
         var cont = e.target.id;
         var abc = cont.split("p");
         cont = abc[1];
-        
-      
-        if (e.keyCode === 40||e.keyCode ===13) {
-          cont = ++cont;
-          document.getElementById("valInp" + cont).focus();
-          return
+
+        if ((e.keyCode === 40 || e.keyCode === 13) && (cont < 3)) {
+            cont = ++cont;
+            document.getElementById("valInp" + cont).focus();
+            return
         }
-        if (e.keyCode === 38 && cont > 0) {
-          cont = cont - 1;
-          document.getElementById("valInp" + cont).focus();
+        if (e.keyCode === 38 && cont > 1) {
+            cont = cont - 1;
+            document.getElementById("valInp" + cont).focus();
+            return
+        }
+        if (e.keyCode === 13 && cont == 3) {
+            document.getElementById("saveKye").click();
+            return
         }
     }
 
@@ -104,31 +119,31 @@ export default function ValidationTest() {
                             id="tooltipForm"
                         >
                             <Row className="row mt-4">
-                                    <Label htmlFor="valInp" className="col-sm-3 col-form-label">
-                                        First name
-                                    </Label>
+                                <Label htmlFor="valInp" className="col-sm-3 col-form-label">
+                                    First name
+                                </Label>
                                 <Col sm={4}>
                                     <Input
                                         type="text"
                                         className="form-control"
                                         id="valInp1"
                                         placeholder="First name"
-                                        // onChange={event => {
-                                        //     onChangeValidation("fnm", event.target.value)
-                                        // }}
-                                        onKeyDown={(event)=>{onKeyPress(event)}}
-                                        valid={validation["fnm"] === true}
+                                        onChange={event => {
+                                            onChangeValidation("valInp1", event.target.value, "text")
+                                        }}
+                                        onKeyDown={(event) => { onKeyPress(event) }}
+                                        valid={validation["valInp1"] === true}
                                         invalid={
-                                            validation["fnm"] !== true &&
-                                            validation["fnm"] !== null
+                                            validation["valInp1"] !== true &&
+                                            validation["valInp1"] !== null
                                         }
                                     />
                                 </Col>
                             </Row>
                             <Row className="row mt-4">
-                                    <Label htmlFor="validationTooltip02" className="col-sm-3 col-form-label">
+                                <Label htmlFor="validationTooltip02" className="col-sm-3 col-form-label">
                                     City name
-                                    </Label>
+                                </Label>
                                 <Col sm={4}>
                                     <Input
                                         type="text"
@@ -136,42 +151,42 @@ export default function ValidationTest() {
                                         id="valInp2"
                                         placeholder="City name"
                                         onChange={event => {
-                                            onChangeValidation("unm", event.target.value,event)
+                                            onChangeValidation("valInp2", event.target.value, "Number")
                                         }}
-                                        onKeyDown={event=>onKeyPress(event)}
+                                        onKeyDown={event => onKeyPress(event)}
                                         on
-                                        valid={validation["unm"] === true}
+                                        valid={validation["valInp2"] === true}
                                         invalid={
-                                            validation["unm"] !== true &&
-                                            validation["unm"] !== null
+                                            validation["valInp2"] !== true &&
+                                            validation["valInp2"] !== null
                                         }
                                     />
                                 </Col>
                             </Row>
                             <Row className="row mt-4">
-                               
-                                    <Label htmlFor="valInp3" className="col-sm-3 col-form-label">
-                                        last name
-                                    </Label>
+
+                                <Label htmlFor="valInp3" className="col-sm-3 col-form-label">
+                                    last name
+                                </Label>
                                 <Col sm={4}>
                                     <Input
                                         type="text"
                                         className="form-control"
-                                        id="valinp3"
+                                        id="valInp3"
                                         placeholder="lastName"
                                         onChange={event => {
-                                            onChangeValidation("lnm", event.target.target,event)
+                                            onChangeValidation("valInp3", event.target.value, "email")
                                         }}
-                                        onKeyDown={event=>onKeyPress(event)}
-                                        valid={validation["lnm"] === true}
+                                        onKeyDown={event => onKeyPress(event)}
+                                        valid={validation["valInp3"] === true}
                                         invalid={
-                                            validation["lnm"] !== true &&
-                                            validation["lnm"] !== null
+                                            validation["valInp3"] !== true &&
+                                            validation["valInp3"] !== null
                                         }
                                     />
                                 </Col>
                             </Row>
-                            <Button color="primary" type="button" onClick={e => {
+                            <Button id='saveKye' color="primary" type="button" onClick={e => {
                                 handleSubmit(e)
                             }}>
                                 Submit form
