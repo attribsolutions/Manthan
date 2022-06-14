c7e082aba1cfe2a52094b30b2806f5365f2472d2
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-06-14 11:49:45 +0530

    Update backend_helper.js

--- src/helpers/backend_helper.js
+++ src/helpers/backend_helper.js
@@ -125,7 +125,7 @@ export const Role_Master_Edit_API=(id)=> get(`${url.ROLE_MASTER_API}/${id}` )// 
 export const Role_Master_Update_API = (data,id) => put(`${url.ROLE_MASTER_API}/${id}`,data )// update api
 
 //Purchase  Order Page api  
-// export const getOrderItems_forOrderPage_ApiCall = () => get(url.GET_ORDER_ITEM)//get api
+export const getOrderItems_forOrderPage_ApiCall = () => get(url.GET_ORDER_ITEM)//get api
 export const submitOrder_From_OrderPage_apiCall = (data) => post(url.ORDER_PAGE_API, data)// post api
 export const editOrderID_forOrderPage_ApiCall = (id) => get(`${url.ORDER_PAGE_API}/${id}`)//Edit Order
 export const UpdateOrder_ID_ApiCall = (data,id) => put(`${url.ORDER_PAGE_API}/${id}`, data)// update api
