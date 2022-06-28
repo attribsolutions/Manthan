1e29a35f09db7eed6239adfb245e72cb61cbbe92
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-06-28 13:39:08 +0530

    Update UserList.js

--- src/pages/Adminisrator/UserRegistrationPages/UserList.js
+++ src/pages/Adminisrator/UserRegistrationPages/UserList.js
@@ -1,4 +1,4 @@
-    import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     Col,
     Modal,
@@ -6,8 +6,8 @@ import {
 } from "reactstrap";
 import "../../../assets/scss/CustomeTable/datatables.scss"
 import {
-    getUser,deleteUser,editUserId,updateSuccess
-} from  "../../../store/Administrator/UserRegistrationRedux/actions";
+    getUser, deleteUser, editUserId, updateSuccess
+} from "../../../store/Administrator/UserRegistrationRedux/actions";
 import Breadcrumbs from "../../../components/Common/Breadcrumb";
 import paginationFactory,
 {
@@ -30,18 +30,18 @@ const UserList = () => {
     const [deleteIn, setDeleteIn] = useState('');
     const [modal_center, setmodal_center] = useState(false);
 
-    const {pages,editData, updateMessage} = useSelector((state) => ({
+    const { pages, editData, updateMessage } = useSelector((state) => ({
         pages: state.User_Registration_Reducer.pages,
-        editData:state.User_Registration_Reducer.editData,
-        updateMessage:state.User_Registration_Reducer.updateMessage,
-      }));
+        editData: state.User_Registration_Reducer.editData,
+        updateMessage: state.User_Registration_Reducer.updateMessage,
+    }));
 
 
     function tog_center() {
         setmodal_center(!modal_center)
     }
-   
-useEffect(() => {
+
+    useEffect(() => {
         dispatch(getUser());
     }, []);
 
@@ -52,7 +52,7 @@ useEffect(() => {
             tog_center()
         }
     }, [updateMessage.Status]);
-    console.log("updateMessage",updateMessage)
+    console.log("updateMessage", updateMessage)
 
     useEffect(() => {
         if (editData.Status === true) {
@@ -64,7 +64,7 @@ useEffect(() => {
     const EditPageHandler = (id) => {
         dispatch(editUserId(id));
     }
-    
+
     //Delete Button Handller
     const deleteHandeler = (id, name) => {
         dispatch(AlertState({
@@ -79,7 +79,7 @@ useEffect(() => {
     const pageOptions = {
         sizePerPage: 15,
         totalSize: pages.length,
-        custom:true,
+        custom: true,
     };
 
     const pagesListColumns = [
@@ -114,7 +114,7 @@ useEffect(() => {
             sort: true,
             formatter: (cellContent, user) =>
                 <>
-                    {(user.isActive) ? 'true ':' false'}
+                    {(user.isActive) ? 'true ' : ' false'}
                 </>,
         },
         {
@@ -122,122 +122,107 @@ useEffect(() => {
             dataField: "isSendOTP",
             sort: true,
             formatter: (cellContent, user) =>
-             <>
-                {( user.isSendOTP)?"true" :"false"}
-            </>
+                <>
+                    {(user.isSendOTP) ? "true" : "false"}
+                </>
         },
         {
             text: "Actions ",
-          
+
             formatter: (cellContent, user) => (
                 <>
-                   <div class="d-flex gap-3" style={{ display: 'flex', justifyContent: 'center' }} >
-    
+                    <div class="d-flex gap-3" style={{ display: 'flex', justifyContent: 'center' }} >
+
                         <buton
                             type="button"
                             onClick={() => {
                                 EditPageHandler(user.ID);
                             }}
                             className="badge badge-soft-primary font-size-12"
-                            >
+                        >
                             <i class="mdi mdi-pencil font-size-18" id="edittooltip"></i>
                         </buton>{" "}
-                       
-                        
-                       <buton
-                        className="badge badge-soft-danger font-size-12"
-                        
-                        onClick={() => {
-                            const deleteID= window.confirm(
-                              "Are you sure you want to Delete ?"
-                                    )
-                           if ( deleteID=== true) {
-                            deleteHandeler(user.ID );
-                                    }
-                                 
-                                }}>
-                        <i class="mdi mdi-delete font-size-18" ></i>
-                    </buton>
-                    <buton
-                        className="badge badge-soft-danger font-size-12"
-                        data-mdb-toggle="tooltip" data-mdb-placement="top" title="Delete Company"
-                        onClick={() => { deleteHandeler(user.ID, user.LoginName); }}
-                    >
-                        <i class="mdi mdi-delete font-size-18" ></i>
-                    </buton>
+
+                        <buton
+                            className="badge badge-soft-danger font-size-12"
+                            data-mdb-toggle="tooltip" data-mdb-placement="top" title="Delete Company"
+                            onClick={() => { deleteHandeler(user.ID, user.LoginName); }}
+                        >
+                            <i class="mdi mdi-delete font-size-18" ></i>
+                        </buton>
                     </div>
                 </>
             ),
         },
     ];
-    
+
     return (
         <React.Fragment>
             <div className="page-content">
-               
-                                    <PaginationProvider
-                                        pagination={paginationFactory(pageOptions)}
-                                    >
-                                        {({ paginationProps, paginationTableProps }) => (
-                                            <ToolkitProvider
-                                                keyField="id"
-                                                data={pages}
-                                                columns={pagesListColumns}
-                                                bootstrap4
-                                                search
-                                            >
-                                                
-                                   {toolkitProps => (
-                                    <React.Fragment>
-                                        <Breadcrumbs
-                                            title={"Count :"}
-                                            breadcrumbItem={"User Registration List"}
-                                            IsButtonVissible={true}
-                                            a={toolkitProps.searchProps}
-                                            breadcrumbCount={pages.length}
-                                            RedirctPath={"/usersMaster"}
-                                        />
-                                        <Row>
-                                            <Col xl="12">
-                                                <div className="table-responsive">
-                                                    <BootstrapTable
-                                                        keyField={"id"}
-                                                        responsive
-                                                        bordered={false}
-                                                        striped={false}
-                                                        classes={
-                                                            "table  table-bordered"
-                                                        }
-                                                        {...toolkitProps.baseProps}
-                                                        {...paginationTableProps}
-                                                    />
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                        <Row className="align-items-md-center mt-30">
-                                            <Col className="pagination pagination-rounded justify-content-end mb-2">
-                                                <PaginationListStandalone
-                                                    {...paginationProps}
-                                                />
-                                            </Col>
-                                        </Row>
-                                    </React.Fragment>
-                                )}
-                                            </ToolkitProvider>
-                                        )}
-                                    </PaginationProvider>
-                                    <Modal
-                            isOpen={modal_center}
-                            toggle={() => { tog_center() }}
-                            size="xl"
+
+                <PaginationProvider
+                    pagination={paginationFactory(pageOptions)}
+                >
+                    {({ paginationProps, paginationTableProps }) => (
+                        <ToolkitProvider
+                            keyField="id"
+                            data={pages}
+                            columns={pagesListColumns}
+                            bootstrap4
+                            search
                         >
-                            <AddUser state={editData.Data} />
-                        </Modal>
-                              </div>
-                      </React.Fragment>
-                  );
-            };
-    
+
+                            {toolkitProps => (
+                                <React.Fragment>
+                                    <Breadcrumbs
+                                        title={"Count :"}
+                                        breadcrumbItem={"User Registration List"}
+                                        IsButtonVissible={true}
+                                        a={toolkitProps.searchProps}
+                                        breadcrumbCount={pages.length}
+                                        RedirctPath={"/usersMaster"}
+                                    />
+                                    <Row>
+                                        <Col xl="12">
+                                            <div className="table-responsive">
+                                                <BootstrapTable
+                                                    keyField={"id"}
+                                                    responsive
+                                                    bordered={false}
+                                                    striped={false}
+                                                    classes={
+                                                        "table  table-bordered"
+                                                    }
+                                                    {...toolkitProps.baseProps}
+                                                    {...paginationTableProps}
+                                                />
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    <Row className="align-items-md-center mt-30">
+                                        <Col className="pagination pagination-rounded justify-content-end mb-2">
+                                            <PaginationListStandalone
+                                                {...paginationProps}
+                                            />
+                                        </Col>
+                                    </Row>
+                                </React.Fragment>
+                            )}
+                        </ToolkitProvider>
+                    )}
+                </PaginationProvider>
+                <Modal
+                    isOpen={modal_center}
+                    toggle={() => { tog_center() }}
+                    size="xl"
+                >
+                    <AddUser state={editData.Data} />
+                </Modal>
+            </div>
+        </React.Fragment>
+    );
+};
+
 export default UserList;
 
 
