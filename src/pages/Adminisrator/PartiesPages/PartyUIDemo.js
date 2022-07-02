0beaf4cdbd56e698377e490f526181dc7f01d75e
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2022-07-02 16:43:42 +0530

    Update PartyUIDemo.js

--- src/pages/Adminisrator/PartiesPages/PartyUIDemo.js
+++ src/pages/Adminisrator/PartiesPages/PartyUIDemo.js
@@ -617,27 +617,27 @@ const PartyUIDemo = (props) => {
     return (
         <React.Fragment>
             <div className="page-content">
-                <Breadcrumbs  breadcrumbItem={"Party UI DEMO "} />
+                <Breadcrumbs breadcrumbItem={"Party UI DEMO "} />
                 <Container fluid>
 
                     <Card >
-                        <CardHeader className="card-header text-center bg-white text-white">
-                            <h4 className="card-title">React Validation - Normal</h4>
-                            <p className="card-title-desc">Provide valuable, actionable feedback to your users with HTML5 form validation–available in all our supported browsers.</p>
+                        <CardHeader className="card-header text-center  text-dark" style={{ backgroundColor: "#dddddd" }}>
+                            <h4 className=" text-center text-black" >React Validation - Normal</h4>
+                            <p className="text-center text-black">Provide valuable, actionable feedback to your users with HTML5 form validation–available in all our supported browsers.</p>
                         </CardHeader>
                         <CardBody>
                             <AvForm>
-                                <Row>
+                                <Row className="d-flex justify-content-center">
 
-                                    <Col lg={12} >
+                                    <Col md={6}  >
                                         <Card >
 
                                             <CardBody style={{ backgroundColor: "whitesmoke" }}>
 
                                                 <FormGroup className="mb-3 ">
                                                     <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Name </Label>
-                                                        <Col sm={3}>
+                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-2 col-form-label">Name </Label>
+                                                        <Col sm={5}>
                                                             <AvField
                                                                 name="Name"
                                                                 placeholder="Please Enter Name"
@@ -648,29 +648,14 @@ const PartyUIDemo = (props) => {
                                                                 id="horizontal-firstname-input"
                                                             />
                                                         </Col>
+
                                                     </Row>
 
                                                 </FormGroup>
                                                 <FormGroup className="mb-3 ">
                                                     <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Description </Label>
-                                                        <Col sm={3}>
-                                                            <AvField
-                                                                name="Description"
-                                                                placeholder="Please Enter Name"
-                                                                type="text"
-                                                                errorMessage="Enter First Name"
-                                                                className="form-control"
-                                                                validate={{ required: { value: true } }}
-                                                                id="horizontal-firstname-input"
-                                                            />
-                                                        </Col>
-                                                    </Row>
-
-                                                </FormGroup><FormGroup className="mb-3 ">
-                                                    <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Module </Label>
-                                                        <Col sm={3}>
+                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-2 col-form-label">Description </Label>
+                                                        <Col sm={5}>
                                                             <AvField
                                                                 name="Name"
                                                                 placeholder="Please Enter Name"
@@ -681,30 +666,15 @@ const PartyUIDemo = (props) => {
                                                                 id="horizontal-firstname-input"
                                                             />
                                                         </Col>
-                                                    </Row>
 
-                                                </FormGroup><FormGroup className="mb-3 ">
-                                                    <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Company </Label>
-                                                        <Col sm={3}>
-                                                            <AvField
-                                                                name="Name"
-                                                                placeholder="Please Enter Name"
-                                                                type="text"
-                                                                errorMessage="Enter First Name"
-                                                                className="form-control"
-                                                                validate={{ required: { value: true } }}
-                                                                id="horizontal-firstname-input"
-                                                            />
-                                                        </Col>
                                                     </Row>
 
                                                 </FormGroup>
-                                                
+
                                                 <FormGroup className="mb-3 ">
                                                     <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Party </Label>
-                                                        <Col sm={3}>
+                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-2 col-form-label">Modules </Label>
+                                                        <Col sm={5}>
                                                             <AvField
                                                                 name="Name"
                                                                 placeholder="Please Enter Name"
@@ -715,50 +685,88 @@ const PartyUIDemo = (props) => {
                                                                 id="horizontal-firstname-input"
                                                             />
                                                         </Col>
-                                                        
+
                                                     </Row>
 
                                                 </FormGroup>
-                                          
-                                                            
+
                                                 <FormGroup className="mb-3 ">
                                                     <Row className="justify-content-md-center">
-                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-1 col-form-label">Party </Label>
-                                                        <Col sm={3}  className="form-check form-switch form-switch-md ">
+                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-2 col-form-label">Mobile </Label>
+                                                        <Col sm={5}>
                                                             <AvField
                                                                 name="Name"
-                                                              
-                                                                type="checkbox"
-                                                             
-                                                            
+                                                                placeholder="Please Enter Name"
+                                                                type="text"
+                                                                errorMessage="Enter First Name"
+                                                                className="form-control"
+                                                                validate={{ required: { value: true } }}
+                                                                id="horizontal-firstname-input"
                                                             />
                                                         </Col>
-                                                        
+
                                                     </Row>
 
                                                 </FormGroup>
-                                          
-                                                <Row className="justify-content-center">
-                                                    <Col sm={2}></Col>
-                                                    <Col sm={2}>
-                                                        <button
-                                                            type="submit"
-                                                            data-mdb-toggle="tooltip" data-mdb-placement="top" title="Save Page"
-                                                            className="btn btn-success w-md"
-                                                        > <i className="fas fa-save me-2"></i> Save
-                                                        </button>
-                                                    </Col>
 
+                                                <FormGroup className="mb-3 ">
+                                                    <Row className="justify-content-md-center">
+                                                        <Label htmlFor="horizontal-firstname-input" className="col-sm-2 col-form-label">Company </Label>
+                                                        <Col sm={5}>
+                                                            <AvField
+                                                                name="Name"
+                                                                placeholder="Please Enter Name"
+                                                                type="text"
+                                                                errorMessage="Enter First Name"
+                                                                className="form-control"
+                                                                validate={{ required: { value: true } }}
+                                                                id="horizontal-firstname-input"
+                                                            />
+                                                        </Col>
 
-                                                </Row>
+                                                    </Row>
+
+                                                </FormGroup>
+
+                                                <FormGroup className="mb-3 ">
+                                                    <Row className="justify-content-md-center">
+                                                        <Col className="col-sm-2 "></Col>
+                                                        <Col sm={5} >
+                                                            <Row>
+                                                            <Label
+                                                                        htmlFor="horizontal-firstname-input"
+                                                                        className="col-sm-4 col-form-label"
+                                                                    >
+                                                                        IsActive
+                                                                    </Label>
+                                                                    <Col md={4} style={{ marginTop: '7px' }} className="form-check form-switch form-switch-sm ">
+                                                                        <AvInput name="isActive" type="checkbox" id="switch1" switch="none" defaultChecked  />
+                                                                        <Label className="me-1" htmlFor="switch1" data-on-label="Yes" data-off-label="No"></Label>
+                                                                    </Col>
+                                                            </Row>
+                                                        </Col>
+                                                    </Row>
+                                                </FormGroup>
+
+                                                <FormGroup className="mb-3 ">
+                                                    <Row className="justify-content-center">
+                                                        <Col sm={1}></Col>
+                                                        <Col sm={2}>
+                                                            <button
+                                                                type="submit"
+                                                                data-mdb-toggle="tooltip" data-mdb-placement="top" title="Save Page"
+                                                                className="btn btn-success w-md"
+                                                            > <i className="fas fa-save me-2"></i> Save
+                                                            </button>
+                                                        </Col>
+
+                                                    </Row>
+                                                </FormGroup >
                                             </CardBody>
 
                                         </Card>
                                     </Col>
                                 </Row>
-
-
-
 
                             </AvForm>
                         </CardBody>
