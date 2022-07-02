ce895ca51081f59b1c2ef9d22d7caa1ba6c094f8
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-07-02 13:12:57 +0530

    Update OrderPage.js

--- src/pages/Purchase/Orders/OrderPage.js
+++ src/pages/Purchase/Orders/OrderPage.js
@@ -48,7 +48,6 @@ const OrderPage = (props) => {
   const currentDate = `${current.getFullYear()}-${month < 10 ? `0${month}` : `${month}`
     }-${current.getDate()}`;
 
-  const [orderDate, setOrderDate] = useState("");
 
   //'IsEdit'--if true then update data otherwise it will perfrom save operation
   const [IsEdit, setIsEdit] = useState(false);
@@ -60,9 +59,10 @@ const OrderPage = (props) => {
 
 
 
+  const [orderDate, setOrderDate] = useState(currentDate);
   const [itemArray, setitemArray] = useState([])
   const [itemCount, setItemCount] = useState(0)
-  const [selectedCustomerState, setSelectedCustomerState] = useState({})
+  const [customerName_dropdownSelect, setCustomerName_dropdownSelect] = useState({})
   const [totalAmountCount, setTotalAmountCount] = useState(0)
 
 
@@ -181,9 +181,9 @@ const OrderPage = (props) => {
         CompanyID: 1,
         DivisionID: 3,
         ExpectedDeliveryDate: orderDate,
-        CreatedOn: orderDate,
+        CreatedOn: currentDate,
         UpdatedBy: 1,
-        UpdatedOn: orderDate,
+        UpdatedOn: currentDate,
         OrderItem: itemArray,// selectedItemArray,
       }),
     };
@@ -354,44 +354,49 @@ const OrderPage = (props) => {
           <Row className="mb-1 border border-black ">
 
             <Col md="2" className="">
-              <FormGroup className="mb-4">
-                <Label htmlFor="validationCustom01">Order Date </Label>
-                <Flatpickr
-                  className="form-control d-block"
-                  defaultValue={currentDate}
-                  placeholder="dd Mm,yyyy"
-                  options={{
-                    altInput: true,
-                    altFormat: "F j, Y",
-                    dateFormat: "Y-m-d"
-                  }}
-                  onChange={(e) => {
-                    // setOrderDate(e.target.value);
-                  }}
-                />
+              <FormGroup className="mb-3 row mt-3 " >
+                <Label className="col-sm-4 mt-2 text-end">Order Date :</Label>
+                <Col className="md-8">
+                  <Flatpickr
+                    className="form-control d-block"
+                    value={orderDate}
+                    placeholder="dd Mm,yyyy"
+                    options={{
+                      altInput: true,
+                      altFormat: "F j, Y",
+                      dateFormat: "Y-m-d"
+                    }}
+                    onChange={(y,e) => {
+                      setOrderDate(e);
+                    }}
+                  />
+                </Col>
               </FormGroup>
             </Col>
 
-            <Col md="2">
-              <FormGroup className="mb-4">
-                <Label htmlFor="validationCustom01">Customer Name </Label>
-                <Col>
+
+
+
+            <Col className={'md-2'}>
+              <FormGroup className="mb-3 row mt-3 " >
+                <Label className="col-sm-5 mt-2 text-end">Customer Name :</Label>
+                <Col className="md-auto ">
                   <ReactSelect
-                    Value={selectedCustomerState}
+                    Value={customerName_dropdownSelect}
                     classNamePrefix="select2-Customer"
                     id={"inp-customerName"}
                     options={CustomerDropdownOptions}
-                    onchange={(e) => { setSelectedCustomerState(e) }}
+                    onchange={(e) => { setCustomerName_dropdownSelect(e) }}
                   />
                 </Col>
               </FormGroup>
             </Col >
 
+            <Col md="3">
 
-            <Col md="2">
-              <FormGroup className="mb-4">
-                <Label htmlFor="validationCustom01">Order Description </Label>
-                <Col>
+              <FormGroup className="mb-3 row mt-3 " >
+                <Label className="col-sm-3  md-6 mt-2 text-end">Descreption :</Label>
+                <Col className="md-6 ">
                   <Input
                     placeholder="Enter Description"
                     id='inp-description'
@@ -400,13 +405,18 @@ const OrderPage = (props) => {
               </FormGroup>
             </Col >
 
-            <Col md={3}></Col>
 
-            <Col md={2}>
+
+            <Col className={'justify-content-md-auto'}></Col>
+
+            <Col className={'justify-content-md-auto'}>
               <Label htmlFor="validationCustom01"> </Label>
-              <div className="bg-soft-primary text-center text-primary external-event col-ls-6 col-form-label border border-danger rounded-2">
-                Order Amount : &nbsp;(&nbsp; {totalAmountCount.toFixed(2)}&nbsp;)
+              <div className="bg-soft-primary text-center text-primary  external-event  col-ls-6 col-form-label rounded-2 align-right">
+                Order Amount : &nbsp;&nbsp; {totalAmountCount.toFixed(2)}&nbsp;
               </div>
+              {/* <h5 className=" text-left text-danger  align-left">
+                Order Amount:&nbsp; {totalAmountCount.toFixed(2)}
+              </h5> */}
             </Col>
 
           </Row>
@@ -422,22 +432,22 @@ const OrderPage = (props) => {
                 >
                   <Thead>
                     <Tr>
-                      <Th data-priority="15">
-                        <Row>
-                          <Col md={3}>Item Name&nbsp;&nbsp;&nbsp;</Col>
-                          <Col md={3}></Col>
-                          <Col ms={3} className="bg-soft-warning text-center text-secondary external-event rounded-2  ">
+                      <Th data-priority="15" className="col-sm-5 text-center">
+                        <Row className="justify-content-md-center">
+                          <Col md={3} className={'align-right'}>Item Name&nbsp;&nbsp;&nbsp;</Col>
+                          {/* <Col md={3}></Col> */}
+                          <Col md={3} className="bg-soft-warning text-center text-secondary external-event rounded-2 align-left}  ">
                             Count : &nbsp;(&nbsp; {itemCount}&nbsp;)
                           </Col>
-                          <Col md={3}></Col>
+                          {/* <Col md={3}></Col> */}
                         </Row>
                       </Th>
-                      <Th data-priority="1">MRP</Th>
-                      <Th data-priority="1">Rate</Th>
-                      <Th data-priority="1" className="col-sm-1">GST %</Th>
-                      <Th data-priority="1" className="col-sm-1">Quantity
+                      <Th data-priority="1" className="text-center">MRP</Th>
+                      <Th data-priority="1" className="text-center">Rate</Th>
+                      <Th data-priority="1" className="text-center" >GST %</Th>
+                      <Th data-priority="1" className="col-sm-1 text-center">Quantity
                       </Th>
-                      <Th data-priority="1" className="col-sm-2" >UOM</Th>
+                      <Th data-priority="1" className="col-sm-2 text-center" >UOM</Th>
                     </Tr>
                   </Thead>
                   <Tbody>
@@ -502,7 +512,7 @@ const OrderPage = (props) => {
                             )}
                           </Td>
 
-                          <Td className="align-bottom">
+                          <Td className="align-bottom text-end">
                             <input
                               type="hidden"
                               id={"lblItemMRP" + key}
@@ -512,7 +522,7 @@ const OrderPage = (props) => {
                             <label style={{ a: "end" }}> {item.MRP} </label>
 
                           </Td>
-                          <Td className="align-bottom">
+                          <Td className="align-bottom text-end">
                             <label > {item.Rate} </label>
                             <input
                               type="hidden"
@@ -522,7 +532,7 @@ const OrderPage = (props) => {
                               autoComplete="false"
                             />
                           </Td>
-                          <Td className="align-bottom">
+                          <Td className="align-bottom text-end">
                             <label > {item.GSTPercentage} % </label>
                             <input
                               type="hidden"
@@ -532,7 +542,7 @@ const OrderPage = (props) => {
                               autoComplete="false"
                             />
                           </Td>
-                          <Td className="align-bottom">
+                          <Td className="align-bottom text-end">
                             <Row style={{ marginTop: "5px", }}>
 
                               <input
