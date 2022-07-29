a795f778ac39c06dd11898836bd8cb1c1f34efef
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-07-29 10:59:19 +0530

    Update RoleAccessList.js

--- src/pages/Adminisrator/RoleAccessPages/RoleAccessList.js
+++ src/pages/Adminisrator/RoleAccessPages/RoleAccessList.js
@@ -112,9 +112,11 @@ const RoleAccessList = (props) => {
         })
 
         setTableListData(Array)
-
+        debugger
 
     }, [GO_buttonPageMasterListForRoleAccess])
+
+
 
     useEffect(() => {
 
@@ -122,7 +124,7 @@ const RoleAccessList = (props) => {
         var eleList = {}
         let NewID = tableListData.length + 1
         let previousData = tableListData
-        debugger
+    
 
         // AddPage_PageMasterListForRoleAccess.map((indexdata) => {
         let indexdata = AddPage_PageMasterListForRoleAccess[0]
@@ -136,7 +138,7 @@ const RoleAccessList = (props) => {
             previousData = previousData.concat(Array)
             setTableListData(previousData)
         }
-
+        debugger
     }, [AddPage_PageMasterListForRoleAccess])
 
     useEffect(() => {
@@ -274,12 +276,12 @@ const RoleAccessList = (props) => {
         }
     }
 
-const ChangeButtonHandeler=()=>{
-    setShowTableOnUI(false);
-    setModule_DropdownSelect('')
-    setPage_DropdownSelect('')
-    setTableListData([])
-}
+    const ChangeButtonHandeler = () => {
+        setShowTableOnUI(false);
+        setModule_DropdownSelect('')
+        setPage_DropdownSelect('')
+        setTableListData([])
+    }
     const saveHandeller = () => {
 
         let selectedItemArray = [];
@@ -288,7 +290,7 @@ const ChangeButtonHandeler=()=>{
 
 
         for (var i = 0; i < tableListData.length; i++) {
-            debugger
+            // debugger
             var moduleName = document.getElementById("ModuleID" + i).value;
             var pageName = document.getElementById("PageID" + i).value;
             var relatedPage = document.getElementById("RelatedPageID" + i).value;
@@ -296,24 +298,36 @@ const ChangeButtonHandeler=()=>{
             var moduleId = parseInt(moduleName)
             var relatedPageID = parseInt(relatedPage)
 
+            var isShowOnMenu= document.getElementById("IsShowOnMenu" + i).checked;
             var isSave = document.getElementById("IsSave" + i).checked
+            var isView = document.getElementById("IsView" + i).checked;
             var isEdit = document.getElementById("IsEdit" + i).checked;
             var isDelete = document.getElementById("IsDelete" + i).checked;
             var isEditSelf = document.getElementById("IsEditSelf" + i).checked;
             var isDeleteSelf = document.getElementById("IsDeleteSelf" + i).checked;
-            var isShow = document.getElementById("IsShowOnMenu" + i).checked;
+            var isPrint = document.getElementById("IsPrint" + i).checked;
             var isTopOfTheDivision = document.getElementById("IsTopOfTheDivision" + i).checked;
-            var isView = document.getElementById("IsView" + i).checked;
 
 
-            if (isSave) roleAccessArray.push({ "PageAccess": 1 });
-            if (isEdit) roleAccessArray.push({ "PageAccess": 2 });
-            if (isDelete) roleAccessArray.push({ "PageAccess": 3 });
-            if (isEditSelf) roleAccessArray.push({ "PageAccess": 4 });
-            if (isDeleteSelf) roleAccessArray.push({ "PageAccess": 5 });
-            if (isShow) roleAccessArray.push({ "PageAccess": 6 });
-            if (isView) roleAccessArray.push({ "PageAccess": 7 });
-            if (isTopOfTheDivision) roleAccessArray.push({ "PageAccess": 8 });
+            if (isShowOnMenu) roleAccessArray.push({ "PageAccess": 1 });
+            if (isSave) roleAccessArray.push({ "PageAccess": 2 });
+            if (isView) roleAccessArray.push({ "PageAccess": 3 });
+            if (isEdit) roleAccessArray.push({ "PageAccess": 4 });
+            if (isDelete) roleAccessArray.push({ "PageAccess": 5});
+            if (isEditSelf) roleAccessArray.push({ "PageAccess": 6 });
+            if (isDeleteSelf) roleAccessArray.push({ "PageAccess": 7 });
+            if (isPrint) roleAccessArray.push({ "PageAccess": 8 });
+            if (isTopOfTheDivision) roleAccessArray.push({ "PageAccess": 9 });
+
+            // 1        IsShowOnMenu
+            // 2        IsSave
+            // 3        IsView
+            // 4        IsEdit
+            // 5        IsDelete
+            // 6        IsEditSelf
+            // 7        IsDeleteSelf
+            // 8        IsPrint
+            // 9        IsTopOfTheDivision
 
             // roleAccessArray.push(roleAccessElement)
 
@@ -353,6 +367,13 @@ const ChangeButtonHandeler=()=>{
         // debugger
 
     };
+
+    console.log("tableListdata",tableListData)
+    console.log("tableHederList",tableHederList)
+    console.log("PageAccess",PageAccess)
+    console.log("GO_buttonPageMasterListForRoleAccess",GO_buttonPageMasterListForRoleAccess)
+    console.log("AddPage_PageMasterListForRoleAccess",AddPage_PageMasterListForRoleAccess)
+
     return (
         <React.Fragment>
             <div className="page-content text-black" >
@@ -526,22 +547,22 @@ const ChangeButtonHandeler=()=>{
 
                                                                     </td>
                                                                     {
-                                                                        PageAccess.map((indexPage)=>{
+                                                                        PageAccess.map((indexPage) => {
 
-                                                                            return(
-                                                                            <td>
-                                                                            {indx[`PageAccess_${indexPage.Name}`] ?
-                                                                                <input type={"checkbox"} id={indexPage.Name + key}
-                                                                                    defaultChecked={indx[`RoleAccess_${indexPage.Name}`] > 0 ? true : false} />
-                                                                                    
-                                                                                : <input type={"hidden"} id={indexPage.Name + key} />
-                                                                            }
-                                                                        </td>
+                                                                            return (
+                                                                                <td>
+                                                                                    {indx[`PageAccess_${indexPage.Name}`] ?
+                                                                                        <input type={"checkbox"} id={indexPage.Name + key}
+                                                                                            defaultChecked={indx[`RoleAccess_${indexPage.Name}`] > 0 ? true : false} />
+
+                                                                                        : <input type={"hidden"} id={indexPage.Name + key} />
+                                                                                    }
+                                                                                </td>
                                                                             )
                                                                         })
                                                                     }
-                                                                    
-                                                                   
+
+
                                                                 </tr>
                                                             )
                                                         })}
