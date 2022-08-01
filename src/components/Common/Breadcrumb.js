4c81725ca849a970e4bb2e68ea0dc258c1da69ff
supriya matale <95119559+supriyamatale@users.noreply.github.com>
2022-08-01 17:33:41 +0530

    Update Breadcrumb.js

--- src/components/Common/Breadcrumb.js
+++ src/components/Common/Breadcrumb.js
@@ -242,7 +242,7 @@ const Breadcrumb = props => {
               </div>
             </div>
           </Col>
-          <Col md={Countsize.length < 10 ? 1 : Countsize.length < 25 ? 2 : 3} className="text-right col-md-1">
+          <Col md={Countsize.length < 10 ? 1 : Countsize.length < 25 ? 2 : 3} className="text-right col-md-2 px-0">
             {
               !(props.breadcrumbCount === undefined)
                 ?
