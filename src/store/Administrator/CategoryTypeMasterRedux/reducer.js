6d79b967544d6368195159573c6c6ca1de768ffd
supriya matale <95119559+supriyamatale@users.noreply.github.com>
2022-08-23 17:47:57 +0530

    catory type redux page update

--- src/store/Administrator/CategoryTypeMasterRedux/reducer.js
+++ src/store/Administrator/CategoryTypeMasterRedux/reducer.js
@@ -1,56 +1,56 @@
-// import {   DELETE_CATEGORY_TYPE_ID_SUCCESS,
-//    EDIT_CATEGORY_TYPE_ID_SUCCESS,
-//     GET_CATEGORY_TYPE_LIST_SUCCESS,
-//      POST_METHOD_HANDLER_FOR_CATEGORY_TYPE_MASTER_API_SUCCESS, 
-//      UPDATE_CATEGORY_TYPE_ID_SUCCESS } from "./actionTypes";
+import {   DELETE_CATEGORY_TYPE_ID_SUCCESS,
+   EDIT_CATEGORY_TYPE_ID_SUCCESS,
+    GET_CATEGORY_TYPE_LIST_SUCCESS,
+     POST_METHOD_HANDLER_FOR_CATEGORY_TYPE_MASTER_API_SUCCESS, 
+     UPDATE_CATEGORY_TYPE_ID_SUCCESS } from "./actionTypes";
 
-//   const INIT_STATE = {
-//     PostData: { Status: false },
-//     categoryTypeListData: [],
-//     deleteMessage: { Status: false },
-//     editData: { Status: false },
-//     updateMessage: { Status: false },
-//   }
+  const INIT_STATE = {
+    PostData: { Status: false },
+    categoryTypeListData: [],
+    deleteMessage: { Status: false },
+    editData: { Status: false },
+    updateMessage: { Status: false },
+  }
   
-//   const  CategoryTypeMasterReducer = (state = INIT_STATE, action) => {
-//     switch (action.type) {
+  const  CategoryTypeMasterReducer = (state = INIT_STATE, action) => {
+    switch (action.type) {
   
-//       case POST_METHOD_HANDLER_FOR_CATEGORY_TYPE_MASTER_API_SUCCESS:
-//         return {
-//           ...state,
-//           PostData: action.payload,
-//         }
+      case POST_METHOD_HANDLER_FOR_CATEGORY_TYPE_MASTER_API_SUCCESS:
+        return {
+          ...state,
+          PostData: action.payload,
+        }
   
       
-//        // get api
-//        case GET_CATEGORY_TYPE_LIST_SUCCESS:
-//         return {
-//           ...state,
-//           categoryTypeListData: action.payload,
-//         }
+       // get api
+       case GET_CATEGORY_TYPE_LIST_SUCCESS:
+        return {
+          ...state,
+          categoryTypeListData: action.payload,
+        }
   
-//       case DELETE_CATEGORY_TYPE_ID_SUCCESS:
-//         return {
-//           ...state,
-//           deleteMessage: action.payload,
-//         };
+      case DELETE_CATEGORY_TYPE_ID_SUCCESS:
+        return {
+          ...state,
+          deleteMessage: action.payload,
+        };
   
-//       case EDIT_CATEGORY_TYPE_ID_SUCCESS:
-//         return {
-//           ...state,
-//           editData: action.payload,
-//         };
+      case EDIT_CATEGORY_TYPE_ID_SUCCESS:
+        return {
+          ...state,
+          editData: action.payload,
+        };
   
-//       // update api
-//       case UPDATE_CATEGORY_TYPE_ID_SUCCESS:
-//         return {
-//           ...state,
-//           updateMessage: action.payload,
-//         };
+      // update api
+      case UPDATE_CATEGORY_TYPE_ID_SUCCESS:
+        return {
+          ...state,
+          updateMessage: action.payload,
+        };
         
-//       default:
-//         return state
-//     }
-//   }
+      default:
+        return state
+    }
+  }
   
-//   export default CategoryTypeMasterReducer  
+  export default CategoryTypeMasterReducer  
