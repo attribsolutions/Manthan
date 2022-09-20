5b065a6f18814a30b287b4f7be7a2ed144c3c9ec
SiddhantPanchwatkar <siddhantpanchwatkar108@gmail.com>
2022-09-20 14:39:52 +0530

    Update PartyType.js

--- src/pages/Adminisrator/PartyTypes/PartyType.js
+++ src/pages/Adminisrator/PartyTypes/PartyType.js
@@ -10,7 +10,7 @@ import {
   Row,
  
 } from "reactstrap";
-import Breadcrumb from "../../../components/Common/Breadcrumb";
+
 import Select from "react-select";
 import { MetaTags } from "react-meta-tags";
 
@@ -22,14 +22,12 @@ import { Link, useHistory } from "react-router-dom";
 import { CommonGetRoleAccessFunction } from "../../../components/Common/CommonGetRoleAccessFunction";
 
 
-import { BreadcrumbShow,AlertState } from "../../../store/actions";
-import { editPartyTypeSuccess, getPartyTypelist, PostPartyTypeAPI, PostPartyTypeAPISuccess, updatePartyTypeID } from "../../../store/Administrator/PartyTypeRedux/action";
+import { BreadcrumbShow,AlertState } from "../../../store/action";
 
-import { BreadcrumbShow, AlertState } from "../../../store/actions";
 import {
   editPartyTypeSuccess,
-  PostPartyTypeAPI,
   PostPartyTypeAPISuccess,
+  getPartyTypelist,
   updatePartyTypeID,
 } from "../../../store/Administrator/PartyTypeRedux/action";
 
@@ -47,8 +45,6 @@ const PartyType = (props) => {
   const [EditData, setEditData] = useState([]);
   const [pageMode, setPageMode] = useState("save");
   const [userPageAccessState, setUserPageAccessState] = useState("");
-  const [partyType_dropdown_Select, setPartyType_dropdown_Select] =
-    useState("");
 
 
     
@@ -101,77 +97,6 @@ const PartyType = (props) => {
     }
   }, [editDataGatingFromList, propsPageMode]);
 
-  useEffect(() => {
-    if (
-      PostAPIResponse.Status === true &&
-      PostAPIResponse.StatusCode === 200 &&
-      !(pageMode === "dropdownAdd")
-    ) {
-    //   setPartyType_dropdown_Select("");
-
-      dispatch(PostPartyTypeAPISuccess({ Status: false }));
-      formRef.current.reset();
-      if (pageMode === "dropdownAdd") {
-        dispatch(
-          AlertState({
-            Type: 1,
-            Status: true,
-            Message: PostAPIResponse.Message,
-          })
-        );
-      } else {
-        dispatch(
-          AlertState({
-            Type: 1,
-            Status: true,
-            Message: PostAPIResponse.Message,
-            RedirectPath: "/PartyTypeList",
-          })
-        );
-      }
-    } else if (
-      PostAPIResponse.Status === true &&
-      !(pageMode === "dropdownAdd")
-    ) {
-      dispatch(PostPartyTypeAPISuccess({ Status: false }));
-      dispatch(
-        AlertState({
-          Type: 4,
-          Status: true,
-          Message: JSON.stringify(PostAPIResponse.Message),
-          RedirectPath: false,
-          AfterResponseAction: false,
-        })
-      )
-    }
-      else if (!(editDataGatingFromList === undefined)) {
-        let relatatedPage = props.relatatedPage
-        userAcc = RoleAccessModifiedinSingleArray.find((inx) => {
-          return (`/${inx.ActualPagePath}` === relatatedPage)
-        })
-  
-      }
-      if (!(userAcc === undefined)) {
-        setUserPageAccessState(userAcc)
-      }
-
-  }, [RoleAccessModifiedinSingleArray])
-
-
-    
-    // This UseEffect 'SetEdit' data and 'autoFocus' while this Component load First Time.
-    useEffect(() => {
-        if (!(userPageAccessState === '')) { document.getElementById("txtName").focus(); }
-        if (!(editDataGatingFromList === undefined)) {
-            setEditData(editDataGatingFromList);
-            setPageMode("edit")
-            dispatch(editPartyTypeSuccess({ Status: false }))
-            dispatch(BreadcrumbShow(editDataGatingFromList.Name))
-        }
-        else if (!(propsPageMode === undefined)) {
-            setPageMode(propsPageMode)
-        }
-    }, [editDataGatingFromList,propsPageMode])
 
     useEffect(() => {
         if ((PostAPIResponse.Status === true) && (PostAPIResponse.StatusCode === 200)&&!(pageMode==="dropdownAdd")) {
@@ -222,12 +147,18 @@ const PartyType = (props) => {
         });
 
       
-    }
-  
+        if (pageMode === "edit") {
+            dispatch(updatePartyTypeID(jsonBody, EditData.id));
+        }
+        else {
+            dispatch(PostPartyTypeAPI(jsonBody));
+        }
+    };
   
 
-  
-  
+// IsEditMode_Css is use of module Edit_mode (reduce page-content marging)
+var IsEditMode_Css = ''
+if ((pageMode === "edit")||(pageMode==="copy")||(pageMode==="dropdownAdd")) { IsEditMode_Css = "-5.5%" };
 
     if (!(userPageAccessState === '')) {
         return (
@@ -356,201 +287,10 @@ const PartyType = (props) => {
         return (
             <React.Fragment></React.Fragment>
         )
-
-    if (pageMode === "edit") {
-      dispatch(updatePartyTypeID(jsonBody, EditData.id));
-    } else {
-      dispatch(PostPartyTypeAPI(jsonBody));
-
-    }
-  };
-
-  // IsEditMode_Css is use of module Edit_mode (reduce page-content marging)
-  var IsEditMode_Css = "";
-  if (pageMode === "edit" || pageMode == "dropdownAdd") {
-    IsEditMode_Css = "-5.5%";
-  }
-
-  if (!(userPageAccessState === "")) {
-    return (
-      <React.Fragment>
-        <div className="page-content" style={{ marginTop: IsEditMode_Css }}>
-          <MetaTags>
-            <title>PartyType| FoodERP-React FrontEnd</title>
-          </MetaTags>
-          <Breadcrumb breadcrumbItem={userPageAccessState.PageHeading} />
-          <Container fluid>
-            <Card className="text-black">
-              <CardHeader
-                className="card-header   text-black"
-                style={{ backgroundColor: "#dddddd" }}
-              >
-                <h4 className="card-title text-black">
-                  {userPageAccessState.PageDescription}
-                </h4>
-                <p className="card-title-desc text-black">
-                  {userPageAccessState.PageDescriptionDetails}
-                </p>
-              </CardHeader>
-
-              <CardBody
-                className=" vh-10 0 text-black"
-                style={{ backgroundColor: "#whitesmoke" }}
-              >
-                <AvForm
-                  onValidSubmit={(e, v) => {
-                    FormSubmitButton_Handler(e, v);
-                  }}
-                  ref={formRef}
-                >
-                  <Row className="">
-                    <Col md={12}>
-                      <Card>
-                        <CardBody style={{ backgroundColor: "whitesmoke" }}>
-                          <Row>
-                            <FormGroup className="mb-2 col col-sm-4 ">
-                              <Label htmlFor="validationCustom01">Name </Label>
-                              <AvField
-                                name="Name"
-                                id="txtName"
-                                value={EditData.Name}
-                                type="text"
-                                placeholder="Please Enter Name"
-                                autoComplete="off"
-                                validate={{
-                                  required: {
-                                    value: true,
-                                    errorMessage: "Please Enter Name",
-                                  },
-                                }}
-                                onChange={(e) => {
-                                  dispatch(BreadcrumbShow(e.target.value));
-                                }}
-                              />
-                            </FormGroup>
-                            <Row>
-                              <Col md="4">
-                                <FormGroup className="mb-3">
-                                  <Label htmlFor="validationCustom01">
-                                    {" "}
-                                    Party Type{" "}
-                                  </Label>
-                                  <Col sm={12}>
-                                    <Select
-                                      value={partyType_dropdown_Select}
-                                      options={PartyType_Dropdown_Options}
-                                      onChange={(e) => {
-                                        PartyType_OnChange_Handler(e);
-                                      }}
-                                    />
-                                  </Col>
-                                </FormGroup>
-                              </Col>
-                            </Row>
-                            <FormGroup>
-                              <Row>
-                                <Col sm={2}>
-                                  <div>
-                                    {pageMode === "edit" ? (
-                                      userPageAccessState.RoleAccess_IsEdit ? (
-                                        <button
-                                          type="submit"
-                                          data-mdb-toggle="tooltip"
-                                          data-mdb-placement="top"
-                                          title="Update Party Type"
-                                          className="btn btn-success w-md"
-                                        >
-                                          <i class="fas fa-edit me-2"></i>Update
-                                        </button>
-                                      ) : (
-                                        <></>
-                                      )
-                                    ) : userPageAccessState.RoleAccess_IsSave ? (
-                                      <button
-                                        type="submit"
-                                        data-mdb-toggle="tooltip"
-                                        data-mdb-placement="top"
-                                        title="Save Party Type"
-                                        className="btn btn-primary w-md"
-                                      >
-                                        {" "}
-                                        <i className="fas fa-save me-2"></i>{" "}
-                                        Save
-                                      </button>
-                                    ) : (
-                                      <></>
-                                    )}
-                                  </div>
-                                </Col>
-                              </Row>
-                            </FormGroup>
-                          </Row>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  </Row>
-                </AvForm>
-              </CardBody>
-            </Card>
-          </Container>
-        </div>
-      </React.Fragment>
-    );
-  } else {
-    return <React.Fragment></React.Fragment>;
-  }
+        }
 };
+    
+export default PartyType
 
-export default PartyType;
 
-export var arrData = [
-  {
-    id: 1,
-    Name: "IsShowOnMenu1",
-    arrData: [
-      {
-        id: 1,
-        Name: "IsShowOnMenu2",
-        arrData: [
-          {
-            id: 1,
-            Name: "IsShowOnMenu3",
-            arrData: [],
-          },
-          {
-            id: 1,
-            Name: "IsShowOnMenu4",
-            arrData: [],
-          },
-        ],
-      },
-      {
-        id: 1,
-        Name: "IsShowOnMenu5",
-        arrData: [
-          {
-            id: 1,
-            Name: "IsShowOnMenu6",
-            arrData: [],
-          },
-          {
-            id: 1,
-            Name: "IsShowOnMenu7",
-            arrData: [
-              {
-                id: 1,
-                Name: "IsShowOnMenu8",
-                arrData: [],
-              },
-              {
-                id: 1,
-                Name: "IsShowOnMenu9",
-                arrData: [],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
+ 
