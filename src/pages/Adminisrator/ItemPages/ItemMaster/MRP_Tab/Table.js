30a4d7bd3040a63c4d59bd8d3e00ff35cbe384b0
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-09-28 11:26:31 +0530

    Item master changes

--- src/pages/Adminisrator/ItemPages/ItemMaster/MRP_Tab/Table.js
+++ src/pages/Adminisrator/ItemPages/ItemMaster/MRP_Tab/Table.js
@@ -1,63 +1,63 @@
-import React, { useState } from 'react';
-import { Button, Table, } from 'reactstrap';
-import { Tbody, Thead } from 'react-super-responsive-table';
+import React, { useState } from "react";
+import { Button, Table } from "reactstrap";
+import { Tbody, Thead } from "react-super-responsive-table";
 
 function MRPTable(props) {
+    
+  const ondeleteHandeler = (ele) => {
+    if (!(ele === 0)) {
+      var fil = props.tableData.filter((i) => {
+        return !(i.id === ele);
+      });
+      props.func(fil);
+    }
+  };
 
-    const ondeleteHandeler = (ele) => {
-
-        if (!(ele === 0)) {
-            var fil = props.tableData.filter((i) => {
-                return !(i.id === ele);
-            });
-            props.func(fil);
-        }
-    };
-
-    const tableRows = props.tableData.map((info) => {
-
-        return (
-            <tr>
-                {/* <td>{info.id}</td> */}
-                <td>{info.DivisionName}</td>
-                <td>{info.PartyName}</td>
-                <td>{info.EffectiveDate}</td>
-                <td>{info.MRP}</td>
-                <td>
-                    <Button
-                        className="badge badge-soft-danger font-size-12 btn btn-danger waves-effect waves-light w-xxs border border-light"
-                        data-mdb-toggle="tooltip" data-mdb-placement="top" title="Delete Party Type"
-                        onClick={(e) => {
-                            ondeleteHandeler(info.id);
-                        }}
-                    >
-                        <i className="mdi mdi-delete font-size-18"></i>
-                    </Button>
-                </td>
-            </tr>
-        );
-    });
-
+  const tableRows = props.tableData.map((info) => {
     return (
-        <>
-            <div>
-            {props.tableData.length > 0 ?
-                    <Table className="table table-bordered table-hover">
-                        <Thead>
-                            <tr>
-                                <th className="col col-sm-3">Division</th>
-                                <th className="col col-sm-3">Party Name</th>
-                                <th className="col col-sm-3">Effective Date</th>
-                                <th className="col col-sm-3">MRP</th>
-                                <th className="col col-sm-3">{"Action"}</th>
-                            </tr>
-                        </Thead>
-                        <Tbody>{tableRows}</Tbody>
-                    </Table>
-                    : null}
-            </div>
-        </>
+      <tr>
+        {/* <td>{info.id}</td> */}
+        <td>{info.DivisionName}</td>
+        <td>{info.PartyName}</td>
+        <td>{info.EffectiveDate}</td>
+        <td>{info.MRP}</td>
+        <td>
+          <Button
+            className="badge badge-soft-danger font-size-12 btn btn-danger waves-effect waves-light w-xxs border border-light"
+            data-mdb-toggle="tooltip"
+            data-mdb-placement="top"
+            title="Delete Party Type"
+            onClick={(e) => {
+              ondeleteHandeler(info.id);
+            }}
+          >
+            <i className="mdi mdi-delete font-size-18"></i>
+          </Button>
+        </td>
+      </tr>
     );
+  });
+
+  return (
+    <>
+      <div>
+        {props.tableData.length > 0 ? (
+          <Table className="table table-bordered table-hover">
+            <Thead>
+              <tr>
+                <th className="col col-sm-3">Division</th>
+                <th className="col col-sm-3">Party Name</th>
+                <th className="col col-sm-3">Effective Date</th>
+                <th className="col col-sm-3">MRP</th>
+                <th className="col col-sm-3">{"Action"}</th>
+              </tr>
+            </Thead>
+            <Tbody>{tableRows}</Tbody>
+          </Table>
+        ) : null}
+      </div>
+    </>
+  );
 }
 
 export default MRPTable;
