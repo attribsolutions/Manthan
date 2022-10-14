f23f16b3bd39af044ff2ef1ccc96d0d580787ace
SiddhantPanchwatkar <siddhantpanchwatkar108@gmail.com>
2022-10-14 15:18:15 +0530

    changes

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -42,6 +42,8 @@ import MarginMasterReducer from "./Administrator/MarginMasterRedux/reducer"
 import TermsAndCondtionsReducer from "./Administrator/TermsAndCondtionsRedux/reducer"
 import GroupTypeReducer from "./Administrator/GroupTypeRedux/reducer"
 import GSTReducer from "./Administrator/GSTRedux/reducer"
+import PartySubPartyReducer from "./Administrator/PartySubPartyRedux/reducer"
+
 const rootReducer = combineReducers({
   Layout,
   Login,
@@ -82,6 +84,7 @@ const rootReducer = combineReducers({
    TermsAndCondtionsReducer,
    GroupTypeReducer,
    GSTReducer,
+   PartySubPartyReducer,
 })
 
 export default rootReducer
