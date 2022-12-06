37c1242530b5cd0e35b67dde30213c85c18f581c
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2022-12-06 16:22:21 +0530

    common master page changes

--- src/components/Common/ComponentRelatedCommonFile/MySearch.js
+++ src/components/Common/ComponentRelatedCommonFile/MySearch.js
@@ -1,32 +1,57 @@
-import { useEffect } from "react";
-
 
 let props1 = { onSearch: function onSearch() { } }
+let input = '';
+let priviousSerach = []
+let pageid = ''
 
-export const mySearchProps = (props) => {
+
+export const mySearchProps = (props, pageID) => {
     props1 = props;
+    pageid = pageID
 };
-let priviousSerach = null
+
+export const defaultSearch = (defaultid) => {
+    debugger
+    const found = priviousSerach.find((i, k) => {
+        return (i.id === defaultid)
+    });
+    let aa = { defaultSearch: '' }
+
+    if (found) {
+        document.getElementById("myInput").value = found.text
+
+        aa = { defaultSearch: found.text }
+    }
+    return aa
+}
+
+
+
+
 export const MySearch = () => {
 
-    // useEffect(() => {
-    //     debugger
-    //     if (priviousSerach) {
-    //         props1.onSearch(len);
-    //         priviousSerach = null
-    //     }
-    // }, [props1])
+    function handleClick(e) {
+        debugger
+        var len = e.target.value
+        input = e.target.value
 
-    let input;
-    const handleClick = () => {
-        var len = input.value
-        priviousSerach = len
+
         if (!(len[0] === "/")) {
-            // const str = len.substring(1);
+
             props1.onSearch(len);
+
+            const found = priviousSerach.find((i, k) => {
+                if ((i.id === pageid)) {
+                    priviousSerach[k] = { id: i.id, text: len }
+                }
+                return (i.id === pageid)
+            });
+
+            if (!found) {
+                priviousSerach.push({ id: pageid, text: len })
+            }
         }
     };
-
     return (
         <div className="app-search d-none d-lg-block " style={{ marginTop: "-3px" }} autocomplete="off">
             <div className="position-relative">
@@ -34,7 +59,6 @@ export const MySearch = () => {
                     id="myInput"
                     className="form-control"
                     placeholder="Search..."
-                    ref={n => input = n}
                     type="text"
                     onChange={handleClick}
                     name="myCountry"
