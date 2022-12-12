d50d3d71a2c960705e9dcef9a929593c131f6db6
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2022-12-12 15:03:19 +0530

    Alert Show if item is used in Work order list

--- src/store/Purchase/BOMRedux/action.js
+++ src/store/Purchase/BOMRedux/action.js
@@ -1,47 +1,59 @@
-import { DELETE_BOM_LIST_PAGE, DELETE_BOM_LIST_PAGE_SUCCESS, EDIT_BOM_LIST_ID, EDIT_BOM_LIST_ID_SUCCESS, GET_BOM_LIST_PAGE, GET_BOM_LIST_PAGE_SUCCESS, GET_ITEM_UNITS_DROPDOWN_API, GET_ITEM_UNITS_DROPDOWN_API_SUCCESS, POST_BOM, POST_BOM_SUCCESS, UPDATE_BOM_LIST, UPDATE_BOM_LIST_SUCCESS } from "./actionTypes";
+import {
+  DELETE_BOM_LIST_PAGE,
+  DELETE_BOM_LIST_PAGE_SUCCESS,
+  EDIT_BOM_LIST_ID,
+  EDIT_BOM_LIST_ID_SUCCESS,
+  GET_BOM_LIST_PAGE,
+  GET_BOM_LIST_PAGE_SUCCESS,
+  GET_ITEM_UNITS_DROPDOWN_API,
+  GET_ITEM_UNITS_DROPDOWN_API_SUCCESS,
+  POST_BOM, POST_BOM_SUCCESS,
+  UPDATE_BOM_LIST,
+  UPDATE_BOM_LIST_SUCCESS
+} from "./actionTypes";
 
 // post api
 export const postBOM = (data) => ({
-    type: POST_BOM,
-    data,
+  type: POST_BOM,
+  data,
 });
 
 export const postBOMSuccess = (data) => ({
-    type: POST_BOM_SUCCESS,
-    payload: data,
+  type: POST_BOM_SUCCESS,
+  payload: data,
 });
 
 // Get Item Units
 export const GetItemUnitsDrodownAPI = (data) => ({
-    type: GET_ITEM_UNITS_DROPDOWN_API,
-    data,
-   
-  });
-  export const GetItemUnitsDrodownAPISuccess = (data) => ({
-    type: GET_ITEM_UNITS_DROPDOWN_API_SUCCESS,
-    payload:data,
-  });
+  type: GET_ITEM_UNITS_DROPDOWN_API,
+  data,
 
-  //get listpage api
+});
+export const GetItemUnitsDrodownAPISuccess = (data) => ({
+  type: GET_ITEM_UNITS_DROPDOWN_API_SUCCESS,
+  payload: data,
+});
+
+//get listpage api
 export const getBOMListPage = (filters) => ({
   type: GET_BOM_LIST_PAGE,
   filters,
 });
 
 export const getBOMListPageSuccess = (data) => ({
-  type:GET_BOM_LIST_PAGE_SUCCESS,
+  type: GET_BOM_LIST_PAGE_SUCCESS,
   payload: data,
 });
 
- // edit api
- export const editBOMList =(id1,pageMode)=>({
-  type:EDIT_BOM_LIST_ID,
-  id1,pageMode
+// edit api
+export const editBOMList = (id1, pageMode) => ({
+  type: EDIT_BOM_LIST_ID,
+  id1, pageMode
 })
 
-export const editBOMListSuccess =(editData)=>({
-  type:EDIT_BOM_LIST_ID_SUCCESS,
- payload:editData,
+export const editBOMListSuccess = (editData) => ({
+  type: EDIT_BOM_LIST_ID_SUCCESS,
+  payload: editData,
 })
 
 export const updateBOMList = (data, id1) => ({
