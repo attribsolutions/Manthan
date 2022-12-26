63d4a4e0bdb6b1809477decb1e0eb5558f3ae692
Patu <prathameshshinde179@gmail.com>
2022-12-26 11:01:13 +0530

    url relation

--- src/routes/urlRalations.js
+++ src/routes/urlRalations.js
@@ -4,6 +4,101 @@ import * as url from "./route_url"
 
 
 
+// ***************************** Utility **************************************//
+
+export const SEARCH_BOX2 = "/SearchBoxSecond"
+export const SEARCH_BOX3 = "/SerachBox3"
+
+// *************************** Administration *******************************//
+
+
+
+export const ModuleMaster = url.MODULE_lIST
+export const ModuleList = url.MODULE_lIST
+
+
+
+export const CompanyMaster = url.COMPANY_lIST
+export const CompanyList = url.COMPANY_lIST
+
+export const UserMaster= url.USER_lIST
+export const UserList = url.USER_lIST
+
+export const RoleMaster = url.ROLE_lIST
+export const RoleList = url.ROLE_lIST
+
+export const RoleAccess = url.ROLEACCESS_lIST
+export const RoleAccessList = url.ROLEACCESS_lIST
+
+export const COPY_ROLEACCESS = "/RoleAccessCopyFunctionality"
+
+// *************************** Master Module **********************************//
+
+
+export const ItemMaster = url.ITEM_lIST
+export const ItemList = url.ITEM_lIST
+
+export const GSTMaster = url.GST_LIST
+export const GSTList = url.GST_LIST
+
+export const CompanyGroupMaster = url.COMPANYGROUP_lIST
+export const CompanyGroupList = url.COMPANYGROUP_lIST
+
+export const EmployeeMaster = url.EMPLOYEE_lIST
+export const EmployeeList = url.EMPLOYEE_lIST
+
+export const EmployeeType = url.EMPLOYEETYPE_lIST
+export const EmployeeTypeList = url.EMPLOYEETYPE_lIST
+
+export const CategoryTypeMaster = url.CATEGORYTYPE_lIST
+export const CategoryTypeList = url.CATEGORYTYPE_lIST
+
+export const DriverMaster = url.DRIVER_lIST
+export const DriverList = url.DRIVER_lIST
+
+export const PartyMaster = url.PARTY_lIST
+export const PartyList = url.PARTY_lIST
+
+export const PartyType = url.PARTYTYPE_lIST
+export const PartyTypeList = url.PARTYTYPE_lIST
+
+export const CategoryMaster = url.CATEGORY_lIST
+export const CategoryList = url.CATEGORY_lIST
+
+
+export const VehicleMaster = url.VEHICLE_lIST
+export const VehicleList = url.VEHICLE_lIST
+
+export const GroupTypeMaster =url.GROUPTYPE_lIST
+export const GroupTypeList = url.GROUPTYPE_lIST
+
+export const GroupMaster = url.GROUP_lIST
+export const GroupList = url.GROUP_lIST
+
+export const PriceMaster = url.PRICE_lIST
+export const PriceList = url.PRICE_lIST
+
+export const MRPMaster = url.MRP_lIST
+export const MRPList = url.MRP_lIST
+
+export const MarginMaster = url.MARGIN_lIST
+export const MarginList = url.MARGIN_lIST
+
+export const PageMaster = url.PAGE_lIST
+export const PageList = url.PAGE_lIST
+
+export const TermsAndCondtionsMaster =url.TERMS_AND_CONDITION_LIST
+export const TermsAndCondtionsList =url.TERMS_AND_CONDITION_LIST
+
+export const PartySubParty = url.PARTY_SUB_PARTY_lIST
+export const PartySubPartyList = url.PARTY_SUB_PARTY_lIST
+
+export const PartyItems =  url.PARTYITEM_LIST
+export const PartyItemsList = url.PARTYITEM_LIST
+
+export const SubGroupList = url.SUBGROUP_LIST
+export const SubGroupMaster = url.SUBGROUP_LIST
+
 //******************************* Purchase Module ************************************//
 
 export const OrderList = url.ORDER_lIST
