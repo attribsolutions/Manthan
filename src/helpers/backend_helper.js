e01efb2719bf7c3e477f430eb5582d01f2590ef2
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-01-03 17:19:10 +0530

    MCUnitDetails url bind in Unit dropdown.

--- src/helpers/backend_helper.js
+++ src/helpers/backend_helper.js
@@ -407,6 +407,8 @@ export const Material_Issue_Get_API = (filters) => post(url.POST_API_FOR_MATERIA
 export const Production_Post_API = (data) => post(url.GO_BUTTON_POST_API_FOR_PRODUCTION, data)// go button postapi
 export const production_get_API = (filters) => post(url.POST_API_FOR_PRODUCTION_LIST, filters)// go button postapi
 export const production_Make_API = (data) => post(url.PRODUCTION_MAKE_API, data)// make production to material Issue postapi
+export const production_UnitDropdown_API = (data) => post(url.PRODUCTION_UNIT_DROPDOWN_API, data)
+
 export {
   getLoggedInUser,
   isUserAuthenticated,
