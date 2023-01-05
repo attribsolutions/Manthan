82ff248795a5ae272a7d477da5e73fe2283f2a92
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-01-05 12:56:16 +0530

    Unit Conversion tab Changes in Item Master.

--- src/pages/Adminisrator/ItemPages/ItemMaster/UnitConversion_Tab/Index.js
+++ src/pages/Adminisrator/ItemPages/ItemMaster/UnitConversion_Tab/Index.js
@@ -1,75 +1,106 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
 import Select from 'react-select'
 import { Tbody, Thead } from 'react-super-responsive-table'
 import { Button, Card, CardBody, Col, FormGroup, Input, Label, Row, Table } from 'reactstrap'
-import { AlertState } from '../../../../../store/actions';
 
 export default function UnitConverstion(props) {
-    const dispatch = useDispatch();
 
     const { pageMode, formValue, TableData = [], BaseUnit = [], } = props.state;
+    
     const { settable, setFormValue } = props
-
 
     const BaseUnit_DropdownOptions = BaseUnit.map((data) => ({
         value: data.id,
         label: data.Name
     }));
 
-
     function baseunitOnchange(event) {
+  
         const val = { ...formValue }
         val["BaseUnit"] = event
         setFormValue(val)
-    }
 
+        if (TableData[0]) {
+            settable([{
+                Conversion: '1',
+                Unit: { value: event.value, label: event.label },
+            }])
+        }
+    }
 
     function addRow_Handler() {
 
         const newarr = [...TableData, {
             Conversion: '',
             Unit: '',
+            POUnit: false,
+            SOUnit: false,
             IsBase: false
         }]
         settable(newarr)
     }
+
     function deleteRow_Handler(key) {
 
         const found = TableData.filter((i, k) => {
             return !(k === key)
         })
-
         settable(found)
     }
-    function baseUnit2_onChange(event, type, key) {
 
-        let newSelectValue = ''
-
+    function baseUnit2_onChange(event, type = '', key) {
+        debugger
         const found = TableData.find((i, k) => {
             return (k === key)
         })
 
-        if (type === "Conversion") {
-            var conv = event.target.value
+        let newSelectValue = ''
 
+        switch (type) {
+            case 'Conversion':
+                // var conv = event.target.value
+                newSelectValue = {
+                    Conversion: event,
+                    Unit: found.Unit,
+                    POUnit: found.POUnit,
+                    SOUnit: found.SOUnit,
+                    IsBase: found.IsBase
+                }
+                break;
 
-            newSelectValue = {
-                Conversion: conv,
-                Unit: found.Unit,
-                IsBase: found.IsBase
-            }
-        }
-        else if (type === 'Unit') {
+            case 'Unit':
+                newSelectValue = {
+                    Conversion: found.Conversion,
+                    Unit: event,
+                    POUnit: found.POUnit,
+                    SOUnit: found.SOUnit,
+                    IsBase: found.IsBase
+                }
+                break;
 
-            newSelectValue = {
-                Conversion: found.Conversion,
-                Unit: event,
-                IsBase: found.IsBase
-            }
+            case 'POUnit':
+                newSelectValue = {
+                    Conversion: found.Conversion,
+                    Unit: found.Unit,
+                    POUnit: event,
+                    SOUnit: found.SOUnit,
+                    IsBase: found.IsBase
+                }
+                break;
+
+            case 'SOUnit':
+                newSelectValue = {
+                    Conversion: found.Conversion,
+                    Unit: found.Unit,
+                    POUnit: found.POUnit,
+                    SOUnit: event,
+                    IsBase: found.IsBase
+                }
+                break;
         }
 
         let newTabArr = TableData.map((index, k) => {
+            debugger
             return (k === key) ? newSelectValue : index
         })
         settable(newTabArr)
@@ -77,8 +108,9 @@ export default function UnitConverstion(props) {
 
     let BaseUnit_DropdownOptions2 = []
     BaseUnit.forEach(myFunction);
+
     function myFunction(item, index, arr) {
- 
+
         if (!(formValue.BaseUnit.label === item.Name)) {
             BaseUnit_DropdownOptions2[index] = {
                 value: item.id,
@@ -101,6 +133,7 @@ export default function UnitConverstion(props) {
                                 <Col md={7}>
                                     <Select
                                         id={`dropUnit-${key}`}
+                                        isDisabled={(key === 0) ? true : false}
                                         placeholder="Select..."
                                         value={index.Unit}
                                         options={BaseUnit_DropdownOptions2}
@@ -110,22 +143,51 @@ export default function UnitConverstion(props) {
                                 < Label className=" col-sm-2 col-form-label">=</Label>
                             </Row>
                         </td>
+
                         <td>
                             <Row>
                                 <Col>
                                     <Input
                                         type="text"
                                         id={`txtConversion${key}`}
-                                        placeholder="Select..."
+                                        placeholder="Select"
+                                        disabled={(key === 0) ? true : false}
                                         autoComplete="off"
                                         defaultValue={index.Conversion}
-                                        onChange={(e) => baseUnit2_onChange(e, "Conversion", key,)}
+                                        onChange={(event) => baseUnit2_onChange(event.target.value, "Conversion", key,)}
                                     >
-
                                     </Input>
                                 </Col>
                                 <Label className=" col-sm-4 col-form-label"> {formValue.BaseUnit.label}</Label>
                             </Row>
+                        </td>
+
+                        <td>
+                            <div>
+                                <Input
+                                    type="radio"
+                                    id={`POUnit-${key}`}
+                                    name="btnradio"
+                                    value={index.POUnit}
+                                    checked={TableData[key].POUnit}
+                                    onChange={(e) => baseUnit2_onChange(e.target.checked, "POUnit", key)}
+                                >
+                                </Input>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div>
+                                <Input
+                                    type="radio"
+                                    id={`SOUnit-${key}`}
+                                    name="btnradio1"
+                                    value={index.SOUnit}
+                                    checked={TableData[key].SOUnit}
+                                    onChange={(e) => baseUnit2_onChange(e.target.checked, "SOUnit", key)}
+                                >
+                                </Input>
+                            </div>
                         </td>
 
                         <td>
@@ -141,11 +203,11 @@ export default function UnitConverstion(props) {
 
                                     </Col>
 
-                                    <Col md={6} style={{marginRight:""}}>
+                                    <Col md={6} style={{ marginRight: "" }}>
                                         <Button
-                                        style={{marginLeft:"-0.6cm"}}
-                                        className=" button_add"
-                                        color="btn btn-outline-primary border-2 font-size-12"
+                                            style={{ marginLeft: "-0.6cm" }}
+                                            className=" button_add"
+                                            color="btn btn-outline-primary border-2 font-size-12"
                                             type="button"
                                             onClick={() => { addRow_Handler(key) }}
                                         >
@@ -167,9 +229,7 @@ export default function UnitConverstion(props) {
 
         });
         return newarr
-
     }
-
 
     return (
         <div>
@@ -198,6 +258,8 @@ export default function UnitConverstion(props) {
                                         <tr>
                                             <th className="col-sm-3">Unit Name</th>
                                             <th className="col-sm-3 text-center">Conversion To Base Unit </th>
+                                            <th className="col-sm-1 text-center">PO Unit</th>
+                                            <th className="col-sm-1 text-center">SO Unit</th>
                                             <th className="col-sm-2">Action</th>
                                         </tr>
                                     </Thead>
