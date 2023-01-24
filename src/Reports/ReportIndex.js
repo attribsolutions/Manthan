04c67fa2f71d4a8cb4f93e8ece13f51bbf0e8b94
Patu <prathameshshinde179@gmail.com>
2023-01-24 11:40:48 +0530

    Invoice Report

--- src/Reports/ReportIndex.js
+++ src/Reports/ReportIndex.js
@@ -1,18 +1,19 @@
+import InvioceReport from "./InvioceReport/Page";
 import ordeRreport from "./OrderReport/Page";
 
-export const order1="order1"
-export const order2="order2"
+export const order1 = "order1"
+export const invoice = "invoice"
 
 
 const generateReport = (resp) => {
-
     switch (resp.ReportType) {
         case order1:
             ordeRreport(resp.Data)
             break;
 
-            case order2:
-            ordeRreport(resp.Data)
+        case invoice:
+            InvioceReport(resp.Data)
+            // ordeRreport(resp.Data)
             break;
 
         default:
