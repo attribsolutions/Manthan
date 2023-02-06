1ed3e612def778e26dfdda191187066e2008d7a2
Patu <prathameshshinde179@gmail.com>
2023-02-06 12:06:47 +0530

    production report

--- src/Reports/ReportIndex.js
+++ src/Reports/ReportIndex.js
@@ -1,10 +1,13 @@
 import InvioceReport from "./InvioceReport/Page";
 import ordeRreport from "./OrderReport/Page";
+import ProductionReport from "./ProductionReport/Page";
+import Production from "./ProductionReport/Page";
+
 
 export const order1 = "order1"
 export const invoice = "invoice"
-
-
+export const Production1 = "Production1"
+debugger
 const generateReport = (resp) => {
     switch (resp.ReportType) {
         case order1:
@@ -14,6 +17,10 @@ const generateReport = (resp) => {
         case invoice:
             InvioceReport(resp.Data)
             // ordeRreport(resp.Data)
+            break;
+
+        case Production1:
+            ProductionReport(resp.Data)
             break;
 
         default:
