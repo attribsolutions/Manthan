c7613ffe74eac85e6c31c3c7987526fafab4ad31
Patu <prathameshshinde179@gmail.com>
2023-02-28 10:15:17 +0530

    challan post api

--- src/pages/Inventory/GRN/GRNAdd.js
+++ src/pages/Inventory/GRN/GRNAdd.js
@@ -558,7 +558,7 @@ const GRNAdd = (props) => {
                 DiscountType: "0",
                 Discount: "0.00",
                 DiscountAmount: "0.00",
-                TaxType: "GST",
+                TaxType:"GST",
 
             }
             let isfound = itemArr.filter(ind => {
