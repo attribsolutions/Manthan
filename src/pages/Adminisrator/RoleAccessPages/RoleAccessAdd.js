5d60c219bc5ebe14ec540667dc67f3a566665b11
Patu <prathameshshinde179@gmail.com>
2023-03-01 11:54:44 +0530

    page master  css

--- src/pages/Adminisrator/RoleAccessPages/RoleAccessAdd.js
+++ src/pages/Adminisrator/RoleAccessPages/RoleAccessAdd.js
@@ -661,7 +661,7 @@ const RoleAccessAdd = (props) => {
                                                 <Col sm={4}>
                                                     <FormGroup className="row">
                                                         <Label className="col-sm-3 p-2 ml-n5">Module</Label>
-                                                        <Col sm={8} style={{ zIndex:"2"}}>
+                                                        <Col sm={8} style={{ zIndex:"3"}}>
                                                             <Select
                                                                 value={module_DropdownSelect}
                                                                 placeholder="select.."
@@ -676,7 +676,7 @@ const RoleAccessAdd = (props) => {
                                                 <Col sm={4}>
                                                     <FormGroup className=" row ">
                                                         <Label className="col-sm-3 p-2">Page</Label>
-                                                        <Col sm={8} style={{ zIndex: "2"}}>
+                                                        <Col sm={8} style={{ zIndex: "3"}}>
                                                             <Select
                                                                 value={page_DropdownSelect}
                                                                 placeholder="select.."
