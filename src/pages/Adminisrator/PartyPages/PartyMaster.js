d89b87fbcf6d2096cedc064842876cf5e0e6b977
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-03-13 15:33:19 +0530

    Party Master Changes.

--- src/pages/Adminisrator/PartyPages/PartyMaster.js
+++ src/pages/Adminisrator/PartyPages/PartyMaster.js
@@ -36,7 +36,7 @@ import {
 import { AlertState, Breadcrumb_inputName } from "../../../store/actions"
 import Tree from "./Tree"
 import AddressDetails_Tab from "./AddressDetailsTab"
-import { breadcrumbReturn, loginUserID, saveDissable } from "../../../components/Common/ComponentRelatedCommonFile/listPageCommonButtons"
+import { breadcrumbReturn, btnIsDissablefunc, loginUserID, saveDissable } from "../../../components/Common/ComponentRelatedCommonFile/listPageCommonButtons"
 import * as url from "../../../routes/route_url";
 import * as pageId from "../../../routes/allPageID"
 import * as mode from "../../../routes/PageMode"
@@ -46,17 +46,17 @@ import { getcompanyList } from "../../../store/Administrator/CompanyRedux/action
 const PartyMaster = (props) => {
     const dispatch = useDispatch();
     const history = useHistory()
-
+debugger
     const [EditData, setEditData] = useState('');
     const [pageMode, setPageMode] = useState(mode.defaultsave);
     const [userPageAccessState, setUserPageAccessState] = useState(11);
     const [activeTab1, setactiveTab1] = useState("1")
     const [modalCss, setModalCss] = useState(false);
-    const [state_DropDown_select, setState_DropDown_select] = useState("");
-    const [district_dropdown_Select, setDistrict_dropdown_Select] = useState("");
-    const [companyList_dropdown_Select, setCompanyList_dropdown_Select] = useState("");
-    const [partyType_dropdown_Select, setPartyType_dropdown_Select] = useState("");
-    const [PriceList_dropdown_Select, setPriceList_dropdown_Select] = useState("");
+    const [state_DropDown_select, setState_DropDown_select] = useState([]);
+    const [district_dropdown_Select, setDistrict_dropdown_Select] = useState([]);
+    const [companyList_dropdown_Select, setCompanyList_dropdown_Select] = useState([]);
+    const [partyType_dropdown_Select, setPartyType_dropdown_Select] = useState([]);
+    const [PriceList_dropdown_Select, setPriceList_dropdown_Select] = useState([]);
     const [AddressDetailsMaster, setAddressDetailsMaster] = useState([]);
     const [PartyPrefix, setPartyPrefix] = useState([]);
 
@@ -183,6 +183,7 @@ const PartyMaster = (props) => {
                 value: PartyTypes[0].id,
                 label: PartyTypes[0].Name
             })
+            dispatch(priceListByPartyAction( PartyTypes[0].id))
         }
     }, [PartyTypes])
 
@@ -280,6 +281,7 @@ const PartyMaster = (props) => {
     }
 
     function PartyType_Dropdown_OnChange_Handller(e) {
+        debugger
         setPartyType_dropdown_Select(e)
         setPriceList_dropdown_Select({ label: '' })
         // setCompanyList_dropdown_Select('')
@@ -314,6 +316,7 @@ const PartyMaster = (props) => {
     }
 
     const FormSubmitButton_Handler = (event, values) => {
+        debugger
 
         if (AddressDetailsMaster.length === 0) {
             dispatch(
@@ -392,6 +395,95 @@ const PartyMaster = (props) => {
             dispatch(postPartyData(jsonBody));
         }
     };
+
+    // const SaveHandler = async (event, values) => {
+    //     debugger
+    //     event.preventDefault();
+    //     const btnId = "Save-PartyMaster"
+    //     btnIsDissablefunc({ btnId, state: true })
+    //     try {
+
+    //         if (AddressDetailsMaster.length === 0) {
+    //             dispatch(
+    //                 AlertState({
+    //                     Type: 4,
+    //                     Status: true,
+    //                     Message: "Address details is required",
+    //                     RedirectPath: false,
+    //                     PermissionAction: false,
+    //                 })
+    //             );
+    //             return;
+    //         }
+
+    //         const data = AddressDetailsMaster.map((index) => {
+    //             return index.IsDefault === true
+    //         })
+
+    //         const count1 = data.filter(value => value === true).length;
+
+    //         if (count1 === 0) {
+    //             dispatch(
+    //                 AlertState({
+    //                     Type: 4,
+    //                     Status: true,
+    //                     Message: "At least one Address Details IsDefault true",
+    //                     RedirectPath: false,
+    //                     PermissionAction: false,
+    //                 })
+    //             );
+    //             return;
+    //         }
+
+    //         const jsonBody = JSON.stringify({
+    //             Name: values.Name,
+    //             PriceList: PriceList_dropdown_Select.value,
+    //             PartyType: partyType_dropdown_Select.value,
+    //             Company: companyList_dropdown_Select.value,
+    //             PAN: values.PAN,
+    //             Email: values.Email,
+    //             MobileNo: values.MobileNo,
+    //             AlternateContactNo: values.AlternateContactNo,
+    //             State: state_DropDown_select.value,
+    //             District: district_dropdown_Select.value,
+    //             Taluka: 0,
+    //             City: 0,
+    //             GSTIN: values.GSTIN,
+    //             MkUpMkDn: values.MkUpMkDn,
+    //             isActive: values.isActive,
+    //             IsDivision: partyType_dropdown_Select.division,
+    //             CreatedBy: loginUserID(),
+    //             CreatedOn: "2022-06-24T11:16:53.165483Z",
+    //             UpdatedBy: loginUserID(),
+    //             UpdatedOn: "2022-06-24T11:16:53.330888Z",
+    //             PartyAddress: AddressDetailsMaster,
+    //             PartyPrefix: [
+    //                 {
+    //                     Orderprefix: values.Orderprefix,
+    //                     Invoiceprefix: values.Invoiceprefix,
+    //                     Grnprefix: values.Grnprefix,
+    //                     Receiptprefix: values.Receiptprefix,
+    //                     Challanprefix: values.Challanprefix,
+    //                     WorkOrderprefix: values.WorkOrderprefix,
+    //                     MaterialIssueprefix: values.MaterialIssueprefix,
+    //                     Demandprefix: values.Demandprefix,
+    //                     IBChallanprefix: values.IBChallanprefix,
+    //                     IBInwardprefix: values.IBInwardprefix
+    //                 }
+    //             ]
+    //         });
+
+
+    //         if (pageMode === mode.edit) {
+    //             dispatch(updatePartyID({ jsonBody, updateId: values.id, btnId }));
+    //         }
+    //         else {
+    //             dispatch(postPartyData({ jsonBody, btnId }));
+    //         }
+
+
+    //     } catch (e) { btnIsDissablefunc({ btnId, state: false }) }
+    // };
 
     var IsEditMode_Css = ''
     if ((pageMode === mode.edit) || (pageMode === mode.copy) || (pageMode === mode.dropdownAdd)) { IsEditMode_Css = "-5.5%" };
@@ -548,8 +640,8 @@ const PartyMaster = (props) => {
                                                                         />
                                                                     </FormGroup>
                                                                 </Col>
-                                                                <Col md="1">  </Col>
 
+                                                                <Col md="1">  </Col>
                                                                 <Col md="3">
                                                                     <FormGroup className="mb-3">
                                                                         <Label htmlFor="validationCustom01">Alternate Contact Number(s)</Label>
@@ -607,6 +699,7 @@ const PartyMaster = (props) => {
                                                                         <Label htmlFor="validationCustom01">Price List </Label>
                                                                         <Input id="Input"
                                                                             value={PriceList_dropdown_Select.label}
+                                                                            autoComplete={"off"}
                                                                             placeholder="Select..."
                                                                             onClick={onclickselect}
                                                                         >
