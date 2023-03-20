e3c5037646def4acf84060e67a02aae087f4cb8e
Patu <prathameshshinde179@gmail.com>
2023-03-20 16:05:03 +0530

    repoty id changes

--- src/pages/Sale/Invoice/InvoiceList.js
+++ src/pages/Sale/Invoice/InvoiceList.js
@@ -139,7 +139,7 @@ const InvoiceList = () => {
     function downBtnFunc(row) {
         debugger
         var ReportType = report.invoice;
-        dispatch(getpdfReportdata(Invoice_1_Edit_API_Singel_Get, ReportType, row.id))
+        dispatch(getpdfReportdata(Invoice_1_Edit_API_Singel_Get, ReportType, {editId: row.id}))
     }
 
     function goButtonHandler(event, IBType) {
