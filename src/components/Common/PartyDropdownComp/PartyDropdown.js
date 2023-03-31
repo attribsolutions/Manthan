94c247257c0ab381abc9e66b01ee363e590c3ec3
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-03-31 16:26:08 +0530

    Party dropdown add in Vehicle List.

---
+++ src/components/Common/PartyDropdownComp/PartyDropdown.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState, } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FormGroup, Label } from "reactstrap";
+import Select from "react-select";
+import { getPartyListAPI } from "../../../store/Administrator/PartyRedux/action";
+
+const PartyDropdownMaster = (props) => {
+
+    const { state, setState, fieldLabel } = props
+
+    const dispatch = useDispatch();
+
+    const { PartyList } = useSelector((state) => ({
+        PartyList: state.PartyMasterReducer.partyList
+    }));
+    useEffect(() => {
+        dispatch(getPartyListAPI())
+    }, []);
+
+    const Party_DropdownOptions = PartyList.map((data) => ({
+        value: data.id,
+        label: data.Name
+    }));
+
+    return (
+        <React.Fragment>
+            <FormGroup className="mb-2 col col-sm-3 ">
+                <Label htmlFor="validationCustom01">{fieldLabel} </Label>
+                <Select
+                    id="Party "
+                    name="Party"
+                    value={state}
+                    isSearchable={false}
+                    className="react-dropdown"
+                    classNamePrefix="dropdown"
+                    options={Party_DropdownOptions}
+                    onChange={(e) => {
+                        setState((i) => {
+                            const a = { ...i }
+                            a.values.Party = e;
+                            return a
+                        })
+                    }}
+                />
+            </FormGroup>
+        </React.Fragment >
+    );
+}
+
+export default PartyDropdownMaster
+
