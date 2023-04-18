4133c7e80b5faa322f3daead1ec3c787a8d89476
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2023-04-18 17:00:01 +0530

    custom validate form create

---
+++ src/CustomValidateForm/CInput.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Input } from 'reactstrap';
+
+const CInput = (prop) => {
+    const { onChange, pattern = '' } = prop
+
+    function on_Change(e) {
+        let val = e.target.value
+        const result = pattern.test(val);
+        if (result) {
+            onChange(e)
+        }
+        else {
+            e.target.value = val.slice(0, -1);
+        }
+    }
+    const param = Object.assign({}, prop, { onChange: on_Change });
+    return (
+        <div>
+            <Input
+                {...param}
+            />
+        </div>
+    )
+}
+
+export default CInput
