0f8053d2e68515240cb014c426ef90a8fced1266
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-04-24 10:04:53 +0530

    Sales return invoice post successfully.

--- src/pages/Sale/Invoice/SalesReturn/SalesReturn.js
+++ src/pages/Sale/Invoice/SalesReturn/SalesReturn.js
@@ -23,7 +23,7 @@ import {
     resetFunction,
 } from "../../../../components/Common/validationFunction";
 import Select from "react-select";
-import { Go_Button, SaveButton } from "../../../../components/Common/CommonButton";
+import { Change_Button, Go_Button, SaveButton } from "../../../../components/Common/CommonButton";
 import { breadcrumbReturnFunc, loginPartyID, currentDate, btnIsDissablefunc, loginUserID, loginCompanyID, convertDatefunc, invertDatefunc } from "../../../../components/Common/CommonFunction";
 import * as pageId from "../../../../routes//allPageID";
 import * as url from "../../../../routes/route_url";
@@ -35,7 +35,7 @@ import { saveSalesReturnMaster, InvoiceNumber, InvoiceNumberSuccess, saveSalesRe
 import CustomTable2 from "../../../../CustomTable2/Table";
 import "./salesReturn.scss";
 import CInput from "../../../../CustomValidateForm/CInput";
-import { floatRegx } from "../../../../CustomValidateForm/RegexPattern";
+import { decimalRegx, floatRegx } from "../../../../CustomValidateForm/RegexPattern";
 import { getpartyItemList } from "../../../../store/Administrator/PartyItemsRedux/action";
 import { SalesReturn_add_button_api, SalesReturn_add_button_api_For_Invoice, SalesReturn_add_button_api_For_Item } from "../../../../helpers/backend_helper";
 import { salesReturnCalculate } from "./SalesCalculation";
@@ -64,7 +64,7 @@ const SalesReturn = (props) => {
 
     const [returnMode, setrRturnMode] = useState(0);
     const [imageTable, setImageTable] = useState([]);
-
+    console.log(returnMode)
     //Access redux store Data /  'save_ModuleSuccess' action data
     const {
         postMsg,
@@ -100,6 +100,12 @@ const SalesReturn = (props) => {
         });
         dispatch(Retailer_List(jsonBody));
     }, []);
+
+    useEffect(() => {
+        if (TableArr.length === 0) {
+            setrRturnMode(0)
+        }
+    }, [TableArr]);
 
     const location = { ...history.location }
     // const hasShowloction = location.hasOwnProperty(mode.editValue)
@@ -215,14 +221,40 @@ const SalesReturn = (props) => {
         setTableArr(newArr)
     }
 
+    function quantityHandler(event, row) {
+        debugger
+        row["Qty"] = event.target.value
+        let input = event.target.value
+
+        if (returnMode === 1) {
+            let v1 = Number(row.Quantity);
+            let v2 = Number(input)
+            if (!(v1 >= v2)) {
+                event.target.value = v1;
+            }
+        }
+        row.Qty = event.target.value
+
+    }
+
     const pagesListColumns = [
         {
             text: "ItemName",
             dataField: "",
             formatter: (cellContent, row, key) => {
-                debugger
                 return (
                     <Label>{row.ItemName.label}</Label>
+
+                )
+            }
+        }, ,
+        {
+            text: "previous Quantity",
+            dataField: "",
+            hidden: returnMode === 2 && true,
+            formatter: (cellContent, row, key) => {
+                return (
+                    <Label>{row.Quantity}</Label>
 
                 )
             }
@@ -240,12 +272,9 @@ const SalesReturn = (props) => {
                         defaultValue={row.Qty}
                         autoComplete="off"
                         type="text"
-                        pattern={floatRegx}
+                        pattern={decimalRegx}
                         className="col col-sm text-end"
-                        // onChange={(event) => {
-                        //     const data = event.target.value
-                        // }}
-                        onChange={(event) => { row.Qty = event.target.value }}
+                        onChange={(event) => quantityHandler(event, row)}
                     />
                 </span>)
             }
@@ -260,11 +289,11 @@ const SalesReturn = (props) => {
                     <Select
                         id={`Unit${key}`}
                         name="Unit"
-                        // defaultValue={row.Calculate}
+                        defaultValue={returnMode === 1 && { value: row.RowData.Unit, label: row.RowData.UnitName }}
                         isSearchable={true}
+                        isDisabled={returnMode === 1 && true}
                         className="react-dropdown"
                         classNamePrefix="dropdown"
-                        // options={ItemList_Options}
                         options={row.ItemUnitDetails}
                         onChange={(event) => {
                             row.Unit = event.value
@@ -279,33 +308,42 @@ const SalesReturn = (props) => {
             dataField: "",
             classes: () => "sales-return-row",
             formatter: (cellContent, row, key) => {
-
-                return (<span style={{ justifyContent: 'center', width: "100px" }}>
-                    <Select
-                        id={`MRP${key}`}
-                        name="MRP"
-                        // defaultValue={row.Calculate}
-                        isSearchable={true}
-                        className="react-dropdown"
-                        classNamePrefix="dropdown"
-                        options={row.ItemMRPDetails}
-                        onChange={(event) => { row.MRP = event.value }}
-                    />
-                </span>)
+                return (
+                    <>
+                        <span style={{ justifyContent: 'center', width: "100px" }}>
+                            {/* {(returnMode === 1) ?
+                                <Label>{row.RowData.MRPValue}</Label>
+                                : */}
+                            <Select
+                                id={`MRP${key}`}
+                                name="MRP"
+                                isSearchable={true}
+                                isDisabled={returnMode === 1 && true}
+                                className="react-dropdown"
+                                classNamePrefix="dropdown"
+                                options={row.ItemMRPDetails}
+                                onChange={(event) => { row.MRP = event.value }}
+                            />
+                            {/* } */}
+                        </span></>)
             }
         },
+
         {
             text: "GST",
             dataField: "",
             classes: () => "sales-return-row",
             formatter: (cellContent, row, key) => {
-
+                // if(row.GSTChange===undefined){
+                //     row.GSTChange={}
+                // }
                 return (<span style={{ justifyContent: 'center', width: "100px" }}>
                     <Select
                         id={`GST${key}`}
                         name="GST"
-                        // defaultValue={row.Calculate}
+                        defaultValue={returnMode === 1 && { value: row.RowData.GST, label: row.RowData.GSTPercentage }}
                         isSearchable={true}
+                        isDisabled={returnMode === 1 && true}
                         className="react-dropdown"
                         classNamePrefix="dropdown"
                         options={row.ItemGSTHSNDetails}
@@ -327,13 +365,11 @@ const SalesReturn = (props) => {
                     <CInput
                         id=""
                         key={row.id}
-                        defaultChecked={row.Rate}
+                        defaultValue={row.Rate}
+                        disabled={returnMode === 1 && true}
                         type="text"
                         pattern={/^-?([0-9]*\.?[0-9]+|[0-9]+\.?[0-9]*)$/}
                         className="col col-sm text-end"
-                        // onChange={(event) => {
-                        //     const data = event.target.value
-                        // }}
                         onChange={(event) => { row.Rate = event.target.value }}
                     />
                 </span>)
@@ -349,10 +385,10 @@ const SalesReturn = (props) => {
                     <Input
                         id=""
                         key={row.id}
-                        defaultChecked={row.BatchCode}
+                        defaultValue={row.RowData.BatchCode}
+                        disabled={returnMode === 1 && true}
                         type="text"
                         className="col col-sm text-center"
-                        // onChange={e => { SelectAll(e.target.checked, row, key) }}
                         onChange={(event) => { row.BatchCode = event.target.value }}
                     />
                 </span>)
@@ -367,7 +403,8 @@ const SalesReturn = (props) => {
                 return (<span style={{ justifyContent: 'center', width: "100px" }}>
                     <Flatpickr
                         name='ReturnDate'
-                        value={currentDate}
+                        defaultValue={returnMode === 1 ? invertDatefunc(row.RowData.BatchDate) : currentDate}
+                        isDisabled={returnMode === 1 ? true : false}
                         className="form-control d-block p-2 bg-white text-dark"
                         placeholder="Select..."
                         options={{
@@ -483,7 +520,7 @@ const SalesReturn = (props) => {
             })
             return
         }
-       
+        debugger
         let resp;
         try {
 
@@ -495,10 +532,13 @@ const SalesReturn = (props) => {
             }
 
             const data = resp.Data.InvoiceItems.map((i) => ({
-                unitOps: i.ItemUnitDetails.map(i => ({ label: i.UnitName, value: i.Unit, BaseUnitQuantity: i.BaseUnitQuantity })),
+                unitOps: (returnMode === 1) ? { label: i.UnitName, value: i.Unit, BaseUnitQuantity: i.BaseUnitQuantity } : i.ItemUnitDetails.map(i => ({ label: i.UnitName, value: i.Unit, BaseUnitQuantity: i.BaseUnitQuantity })),
                 MRPOps: i.ItemMRPDetails.map(i => ({ label: i.MRPValue, value: i.MRP })),
                 GSTOps: i.ItemGSTDetails.map(i => ({ label: i.GSTPercentage, value: i.GST })),
-                ItemName: { label: i.ItemName, value: i.Item }
+                ItemName: { label: i.ItemName, value: i.Item },
+                Quantity: i.Quantity,
+                Rate: i.Rate,
+                RowData: i
             }))
 
             const itemArr = [...TableArr]
@@ -510,24 +550,19 @@ const SalesReturn = (props) => {
                     ItemMRPDetails: i.MRPOps,
                     ItemGSTHSNDetails: i.GSTOps,
                     ItemName: i.ItemName,
+                    Quantity: i.Quantity,
+                    Rate: i.Rate,
+                    gstPercentage: i.RowData.GSTPercentage,
+                    RowData: i.RowData
                 })
             })
-            setTableArr(itemArr)
 
-            // i.push(setTableArr([...TableArr, {
-            //     id: TableArr.length + 1,
-            //     ItemUnitDetails: i.unitOps,
-            //     ItemMRPDetails: i.MRPOps,
-            //     ItemGSTHSNDetails: i.GSTOps,
-            //     ItemName: i.ItemName,
-            // }]))
+            setTableArr(itemArr)
 
             setState((i) => {
                 let a = { ...i }
                 a.values.ItemName = ""
-                a.values.InvoiceNumber = ""
                 a.hasValid.ItemName.valid = true;
-                a.hasValid.InvoiceNumber.valid = true;
                 return a
             })
         } catch (w) { }
@@ -602,23 +637,23 @@ const SalesReturn = (props) => {
         let grand_total = 0;
         const ReturnItems = TableArr.map((i) => {
 
-            const calculate = salesReturnCalculate(i)
+            const calculate = salesReturnCalculate({ Rate: i.Rate, Qty: i.Qty, gstPercentage: returnMode === 1 ? i.gstPercentage : i.GST })
 
             grand_total = grand_total + Number(calculate.tAmount)
 
             return ({
                 Item: i.ItemName.value,
                 Quantity: i.Qty,
-                Unit: i.Unit,
+                Unit: returnMode === 1 ? i.RowData.Unit : i.Unit,
                 BaseUnitQuantity: i.BaseUnitQuantity,
-                BatchCode: i.BatchCode,
+                BatchCode: returnMode === 1 ? i.RowData.BatchCode : i.BatchCode,
                 BatchDate: i.BatchDate,
                 Amount: calculate.tAmount,
-                MRP: i.MRP,
+                MRP: returnMode === 1 ? i.RowData.MRP : i.MRP,
                 Rate: i.Rate,
                 BasicAmount: calculate.baseAmt,
                 GSTAmount: calculate.gstAmt,
-                GST: i.GST_ID,
+                GST: returnMode === 1 ? i.RowData.GST : i.GST_ID,
                 CGST: calculate.CGST,
                 SGST: calculate.SGST,
                 IGST: 0,
@@ -823,9 +858,7 @@ const SalesReturn = (props) => {
                                                     setrRturnMode(2)
                                                 }}
                                             />
-                                            {/* {isError.ItemName.length > 0 && (
-                                                <span className="text-danger f-8"><small>{isError.ItemName}</small></span>
-                                            )} */}
+
                                         </Col>
 
                                         <Col sm="1" className="mx-4 mt-1 ">
@@ -845,7 +878,7 @@ const SalesReturn = (props) => {
                                                 id="InvoiceNumber "
                                                 name="InvoiceNumber"
                                                 value={values.InvoiceNumber}
-                                                isDisabled={(returnMode === 2) ? true : false}
+                                                isDisabled={((returnMode === 2) || (TableArr.length > 0)) ? true : false}
                                                 isSearchable={true}
                                                 className="react-dropdown"
                                                 classNamePrefix="dropdown"
@@ -855,68 +888,36 @@ const SalesReturn = (props) => {
                                                     setrRturnMode(1)
                                                 }}
                                             />
-                                            {/* {isError.InvoiceNumber.length > 0 && (
-                                                <span className="text-danger f-8"><small>{isError.InvoiceNumber}</small></span>
-                                            )} */}
+
                                         </Col>
                                         <Col sm="1" className="mx-4 mt-1 ">{/*Go_Button  */}
 
-                                            <Button type="button" color="btn btn-outline-primary border-1 font-size-11 text-center"
-                                                onClick={(e,) => AddPartyHandler(e, "1")}
-                                            >        <i > </i>Add</Button>
+                                            <Col sm="1" className="mx-4 ">                      {/*Go_Button  */}
+                                                {(pageMode === mode.defaultsave) ?
+                                                    (TableArr.length === 0) || (returnMode === 2) ?
+                                                        <Button type="button" color="btn btn-outline-primary border-1 font-size-11 text-center"
+                                                            onClick={(e,) => AddPartyHandler(e, "1")}
+                                                        >        <i > </i>Add</Button>
+                                                        :
+                                                        <Change_Button onClick={(e) => {
+                                                            setTableArr([])
+                                                            setState((i) => {
+                                                                let a = { ...i }
+                                                                a.values.InvoiceNumber = ""
+                                                                a.hasValid.InvoiceNumber.valid = true;
+                                                                return a
+                                                            })
+                                                        }} />
+                                                    : null
+                                                }
 
+                                            </Col>
                                         </Col>
                                     </FormGroup>
                                 </Col >
 
                             </Row>
                         </div>
-
-                        {/* <PaginationProvider
-                            pagination={paginationFactory(pageOptions)}
-                        >
-                            {({ paginationProps, paginationTableProps }) => (
-                                <ToolkitProvider
-
-                                    keyField="id"
-                                    data={[...TableArr]}
-                                    columns={pagesListColumns}
-
-                                    search
-                                >
-                                    {toolkitProps => (
-                                        <React.Fragment>
-                                            <div className="table">
-                                                <BootstrapTable
-                                                    keyField={"id"}
-                                                    bordered={true}
-                                                    striped={false}
-                                                    noDataIndication={<div className="text-danger text-center ">Record Not available</div>}
-                                                    classes={"table align-middle table-nowrap table-hover"}
-                                                    headerWrapperClasses={"thead-light"}
-
-                                                    {...toolkitProps.baseProps}
-                                                    {...paginationTableProps}
-                                                />
-                                                {countlabelFunc(toolkitProps, paginationProps, dispatch, "MRP")}
-                                                {mySearchProps(toolkitProps.searchProps)}
-                                            </div>
-
-                                            <Row className="align-items-md-center mt-30">
-                                                <Col className="pagination pagination-rounded justify-content-end mb-2">
-                                                    <PaginationListStandalone
-                                                        {...paginationProps}
-                                                    />
-                                                </Col>
-                                            </Row>
-                                        </React.Fragment>
-                                    )
-                                    }
-                                </ToolkitProvider>
-                            )
-                            }
-
-                        </PaginationProvider> */}
 
                         <CustomTable2
                             data={TableArr}
