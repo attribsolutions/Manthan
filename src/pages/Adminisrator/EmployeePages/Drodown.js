b3981198947f136feef3f08e80dbf68d8897d9dc
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2023-05-13 14:50:13 +0530

    Update Drodown.js

--- src/pages/Adminisrator/EmployeePages/Drodown.js
+++ src/pages/Adminisrator/EmployeePages/Drodown.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { Button, Modal } from 'reactstrap'
 import * as mode from "../../../routes/PageMode"
-import * as url from "../../../routes/route_url";
 
 const AddMaster = (props) => {
 
-    const [isOpen, setIsOpen] = useState(false)
-    let { bredcrumbItemName = '' } = useSelector((state) => ({
+    const [isOpen, setIsOpen] = useState(false);
+    const [pageHeading, setPageHeading] = useState('')
+    const { bredcrumbItemName = '', userAccess = [] } = useSelector((state) => ({
         bredcrumbItemName: state.BreadcrumbReducer.bredcrumbItemName,
+        userAccess: state.Login.RoleAccessUpdateData,
     }));
+
+    useEffect(() => {
+        let pagefond = userAccess.find((inx) => {
+            return (`/${inx.ActualPagePath}` === props.masterPath)
+        });
+        if (!(pagefond === undefined)) {
+            setPageHeading(pagefond.PageHeading)
+        }
+
+    }, [userAccess, props]);
 
     return (
         <>
             <div>
                 <Button
                     className=" rounded_Add_Btn "
-                    // style={{ borderRadius: "25px !important" }}
                     color="btn btn-outline-primary border-2 font-size-12"
                     type="button"
                     onClick={() => { setIsOpen(true) }}
@@ -30,23 +40,18 @@ const AddMaster = (props) => {
                     }}
                     size="xl"
                 >
-                    <header  style={{ zIndex: "1" }}  >
-                                    <div>
-                                        <label className="font-size-18  col-ls-6 col-form-label text-black" style={{ marginLeft: "6px" }}>
-                                            {"pageHeading"}</label>
-                                        {(bredcrumbItemName.length > 0) ?
-                                            <label className="font-size-24 form-label  text-nowrap bd-highlight text-primary"
-                                                style={{ paddingLeft: "7px", color: "#5156be" }} >&nbsp;/&nbsp;{bredcrumbItemName}</label>
-                                            : null
-                                        }
-                                    </div>
-
-                             
+                    <header style={{ "zIndex": "1", "paddingTop": "9px", "marginBottom": "-8px" }}  >
+                        <div>
+                            <label className="font-size-18  col-ls-6 col-form-label text-black" style={{ marginLeft: "6px" }}>
+                                {pageHeading}</label>
+                            {(bredcrumbItemName.length > 0) ?
+                                <label className="font-size-24 form-label  text-nowrap bd-highlight text-primary"
+                                    style={{ paddingLeft: "7px", color: "#5156be" }} >&nbsp;/&nbsp;{bredcrumbItemName}</label>
+                                : null
+                            }
+                        </div>
                     </header>
-                    <div id="123456" style={{
-                        marginBottom: "-126px",
-                        marginTop: "-88px"
-                    }}>
+                    <div style={{ margin: "-102px  -12px -136px" }}>
 
                         <props.masterModal
                             masterPath={props.masterPath}
