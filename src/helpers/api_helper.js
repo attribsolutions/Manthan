19cf14059aa4173d9f85f06ac47f8e7b08c8cd97
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-05-29 13:11:34 +0530

    Update api_helper.js

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -3,9 +3,9 @@ import { CheckAPIResponse, CommonConsole } from "../components/Common/CommonFunc
 
 
 
-const API_URL = "http://192.168.1.114:8000"
+// const API_URL = "http://192.168.1.114:8000"
 
-// const API_URL = "http://117.248.109.234:8000"
+const API_URL = "http://117.248.109.234:8000"
 
 const axiosApi = axios.create({
   baseURL: API_URL,
