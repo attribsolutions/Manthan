2798b254428678deef94d02f0cf25ba1060ac590
Patu <prathameshshinde179@gmail.com>
2023-05-30 10:42:47 +0530

    profile menu

--- src/CustomValidateForm/RegexPattern.js
+++ src/CustomValidateForm/RegexPattern.js
@@ -2,3 +2,9 @@ export const floatRegx = /^-?([0-9]*\.?[0-9]+|[0-9]+\.?[0-9]*)$/
 
 export const decimalRegx = /^\d*(\.\d{0,2})?$/
 export const onlyNumberRegx = /^\d+$/
+export const passwordRgx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/
+
+
+
+
+
