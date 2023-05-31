f55a83c4cf675f83b11cc976b32de489a1fdd897
Patu <prathameshshinde179@gmail.com>
2023-05-31 16:45:21 +0530

    sap ledger changes

--- src/store/Report/SapLedger Redux/saga.js
+++ src/store/Report/SapLedger Redux/saga.js
@@ -1,28 +1,50 @@
+
 import { call, put, takeEvery } from "redux-saga/effects";
 import {
-  ChangePassword_Succes,SapLedger_Go_Button_API,SapLedger_Go_Button_API_Success,s, getExcel_Button_API_Success
+  SapLedger_Go_Button_API_Success, getExcel_Button_API_Success
 } from "./action";
 import {
-  ChangePassword_API, GetExcelButton, PartyLedger_API,
+ GetExcelButton, PartyLedger_API,
 } from "../../../helpers/backend_helper";
 import {
-  CHANGE_PASSWORD, GET_EXCELBUTTON_API, GO_BUTTON_API_SAP_LEDGER,
+ GET_EXCELBUTTON_API, GO_BUTTON_API_SAP_LEDGER,
 } from "./actionType";
 import { CommonConsole } from "../../../components/Common/CommonFunction";
 
 
-function* goBtn_Get_API_GenFun( { filters } ) {
+function* goBtn_Get_API_GenFun({ filters }) {
 
   try {
-   
-      const response = yield call(PartyLedger_API, filters);
-      
-      // response.Data.map((index) => {
-      //     index["selectCheck"] = false
-      //     return index
-      // });
 
-      yield put(SapLedger_Go_Button_API_Success(response ));
+    const response = yield call(PartyLedger_API, filters);
+
+    let TotalDebitAmount = 0
+    let TotalCreditAmount = 0
+
+    const newresponse = yield response.data.map((i, key) => {
+      i.id = key + 1
+      if (i.DebitCredit === "S") {
+        i.Debit_Amount = i.Amount
+        TotalDebitAmount = Number(TotalDebitAmount) + Number(i.Debit_Amount)
+      }
+      if (i.DebitCredit === "H") {
+        i.Credit_Amount = i.Amount
+        TotalCreditAmount = Number(TotalCreditAmount) + Number(i.Credit_Amount)
+      }
+
+      return i
+
+    })
+    newresponse.push({
+      id: response.length - 1,
+      PostingDate: "Total",
+      Credit_Amount: TotalCreditAmount,
+      Debit_Amount: TotalDebitAmount
+    })
+    response.data = newresponse
+
+    yield put(SapLedger_Go_Button_API_Success(response));
+
   } catch (error) { CommonConsole(error) }
 }
 
