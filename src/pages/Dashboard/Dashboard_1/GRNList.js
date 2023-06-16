d3e4511464a0e8182dff503c48a861ab8f4f06e6
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-06-16 15:10:24 +0530

    Dashboard extra code remove.

--- src/pages/Dashboard/Dashboard_1/GRNList.js
+++ src/pages/Dashboard/Dashboard_1/GRNList.js
@@ -97,6 +97,7 @@ export default function InvoiceForGRN() {
         {
             text: "InvoiceAmount",
             dataField: "OrderAmount",
+            align:"right"
         },
         {
             text: "Action",
