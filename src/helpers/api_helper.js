a20dd2487092ba377e5efb6da66a1757ffb52d98
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2023-06-19 13:28:29 +0530

    build file

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -8,6 +8,9 @@ const API_URL = "http://192.168.1.114:8000"
 // const API_URL = "http://117.248.109.234:8000"
 // const API_URL = "http://10.4.5.64:8000"
 
+// const API_URL = "http://cbmfooderp.com:8000"
+
+
 const axiosApi = axios.create({
   baseURL: API_URL,
 })
