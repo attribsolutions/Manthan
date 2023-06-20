43fabe7cf5eaf766902fabbb6690bd1d00dba1b9
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2023-06-20 11:55:14 +0530

    build file

--- src/components/Common/TableCommonFunc.js
+++ src/components/Common/TableCommonFunc.js
@@ -137,7 +137,6 @@ const DynamicColumnHook = ({ tableList = [], pageField = '', lastColumn, secondL
                 if (islastCol) {
                     columns.push(lastColumn())
                 }
-
             }
         })
         if (columns.length > 0) {
