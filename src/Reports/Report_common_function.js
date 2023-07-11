26567fc41764622470279f8c836ad2e91b9915ff
Patu <prathameshshinde179@gmail.com>
2023-07-11 17:53:59 +0530

    reprt changes

--- src/Reports/Report_common_function.js
+++ src/Reports/Report_common_function.js
@@ -99,3 +99,79 @@ export function toWords(number) {
   rupeesWords += ' Only';
   return rupeesWords;
 }
+
+
+export function toWordswithoutRS(number) {
+  // Define word mappings
+  const units = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  const tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+  const teens = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+
+  // Function to convert a given three-digit number to words
+  function convertThreeDigits(num) {
+    let word = '';
+    const hundred = Math.floor(num / 100);
+    num %= 100;
+    if (hundred !== 0) {
+      word += units[hundred] + ' Hundred ';
+    }
+    if (num >= 10 && num <= 19) {
+      word += teens[num - 10] + ' ';
+    } else {
+      const ten = Math.floor(num / 10);
+      num %= 10;
+      if (ten !== 0) {
+        word += tens[ten] + ' ';
+      }
+      if (num !== 0) {
+        word += units[num] + ' ';
+      }
+    }
+    return word.trim();
+  }
+
+  // Check if the number is zero
+  if (number === 0) {
+    return 'Zero Rupees Only';
+  }
+
+  // Separate the whole and decimal parts of the number
+  let wholePart = Math.floor(number);
+  let decimalPart = Math.round((number - wholePart) * 100);
+
+  // Convert the whole part to words
+  const crore = Math.floor(wholePart / 10000000);
+  wholePart %= 10000000;
+  const lakh = Math.floor(wholePart / 100000);
+  wholePart %= 100000;
+  const thousand = Math.floor(wholePart / 1000);
+  const remaining = wholePart % 1000;
+
+  let rupeesWords = '';
+
+  if (crore !== 0) {
+    rupeesWords += convertThreeDigits(crore) + ' Crore ';
+  }
+
+  if (lakh !== 0) {
+    rupeesWords += convertThreeDigits(lakh) + ' Lakh ';
+  }
+
+  if (thousand !== 0) {
+    rupeesWords += convertThreeDigits(thousand) + ' Thousand ';
+  }
+
+  if (remaining !== 0) {
+    rupeesWords += convertThreeDigits(remaining);
+  }
+
+  // Construct the final representation
+  rupeesWords += ' ';
+
+  if (decimalPart !== 0) {
+    rupeesWords += ' and ' + convertThreeDigits(decimalPart) + ' Paise';
+  }
+
+  rupeesWords += ' Only';
+  return rupeesWords;
+}
