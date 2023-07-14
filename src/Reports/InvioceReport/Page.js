adbeec7913a399197aa275c5962f085ae585905e
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2023-07-14 13:00:09 +0530

    chnage in report file

--- src/Reports/InvioceReport/Page.js
+++ src/Reports/InvioceReport/Page.js
@@ -1,66 +1,73 @@
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-import * as style from './ReportStyle'
-// import { Data } from "./DemoData";
+import * as style from './ReportStyle';
 import { compareGSTINState } from "../../components/Common/CommonFunction";
 
-
 const pageHeder = (doc, data) => {
-    style.pageBorder(doc, data);
-    style.pageHeder(doc, data);     //Title
-    style.reportHeder1(doc, data);
-    style.reportHeder2(doc, data);
-    style.reportHeder3(doc, data);    //Invoice ID , Date  
-    return true
+  style.pageBorder(doc, data);
+  style.pageHeder(doc, data);     //Title
+  style.reportHeder1(doc, data);
+  style.reportHeder2(doc, data);
+  style.reportHeder3(doc, data);    //Invoice ID , Date
+  return true;
 };
-const reportBody = (doc, data) => {
 
-    const isIGST = compareGSTINState(data.CustomerGSTIN, data.PartyGSTIN)
-    if (isIGST) {
-        style.tableBodyWithIGST(doc, data);//table Body
-    } else {
-        style.tableBody(doc, data);
-    }
-}
+const reportBody = (doc, data) => {
+  const isIGST = compareGSTINState(data.CustomerGSTIN, data.PartyGSTIN);
+  if (isIGST) {
+    style.tableBodyWithIGST(doc, data); //table Body
+  } else {
+    style.tableBody(doc, data);
+  }
+};
+
 function pageFooter(doc, data) {
-    style.pageFooter(doc, data);
-    style.reportFooter(doc, data);
+  style.pageFooter(doc, data);
+  style.reportFooter(doc, data);
 }
 
 const InvioceReport = async (data) => {
-
-    if (data.InvoiceUploads.length > 0) {
-        if (data.InvoiceUploads[0].QRCodeUrl !== null) {
-            data["isQR"] = true
-        } else {
-            data["isQR"] = false
-        }
+  if (data.InvoiceUploads.length > 0) {
+    if (data.InvoiceUploads[0].QRCodeUrl !== null) {
+      data["isQR"] = true;
+    } else {
+      data["isQR"] = false;
     }
-    var doc = new jsPDF('p', 'pt', 'a4');
+  }
+  var doc = new jsPDF('p', 'pt', 'a4');
 
-    // if (data.InvoiceUploads.length > 0) {
-    //     const url = data.InvoiceUploads[0].QRCodeUrl;
-    //     let desiredPart = null;
+  if (data.InvoiceUploads.length > 0) {
+    const url = data.InvoiceUploads[0].QRCodeUrl;
+    let desiredPart = null;
 
-    //     try {
-    //         const urlObject = new URL(url);
-    //         desiredPart = urlObject.pathname;
-    //     } catch (w) { }
+    try {
+      const urlObject = new URL(url);
+      desiredPart = urlObject.pathname;
+    } catch (w) {}
 
-    //     const image = await loadImage(url);
-    //     if (image) {
-    //         doc.addImage(image, 'JPEG', 323, 18, 83, 83);
-    //     }
-    // }
-    // // Function to load an image asynchronously
-    // function loadImage(url) {
-    //     return new Promise((resolve, reject) => {
-    //         const img = new Image();
-    //         img.onload = () => resolve(img);
-    //         img.onerror = reject;
-    //         img.src = url;
-    //     });
-    // }
+    const image = await loadImage(`/E_invoiceQRCode${desiredPart}`);
+    if (image) {
+      doc.addImage(image, 'JPEG', 323, 18, 83, 83);
+    } else {
+      doc.text('Image Not Found', 323, 18);
+    }
+  }
+
+  function loadImage(url) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+      img.src = url;
+    });
+  }
+
+  pageHeder(doc, data);
+  reportBody(doc, data);
+  pageFooter(doc, data);
+
+
+
 
     pageHeder(doc, data);
     reportBody(doc, data);
