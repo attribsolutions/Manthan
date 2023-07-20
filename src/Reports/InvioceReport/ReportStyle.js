f49fc93c927e7f56c58f6bf3001f0302e2e8efac
SiddhantPanchwatkar <95119559+supriyamatale@users.noreply.github.com>
2023-07-20 17:30:44 +0530

    report

--- src/Reports/InvioceReport/ReportStyle.js
+++ src/Reports/InvioceReport/ReportStyle.js
@@ -238,7 +238,7 @@ export const reportHeder3 = (doc, data) => {
 export const reportFooter = (doc, data) => {
 
     let stringNumber = toWords(Number(data.GrandTotal))
-    doc.addImage(upi_qr_code, 'PNG', 359, 747, 75, 65)
+    // doc.addImage(upi_qr_code, 'PNG', 359, 747, 75, 65)
     doc.setDrawColor(0, 0, 0);
     doc.line(570, 730, 30, 730);//horizontal line Footer 1
     doc.line(435, 745, 30, 745);//horizontal line Footer 2
