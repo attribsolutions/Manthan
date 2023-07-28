986689939f7cc921dd596050b8bc960487e6cbca
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-07-28 15:12:06 +0530

    Party error solve.

--- src/components/Common/PartyDropdown.js
+++ src/components/Common/PartyDropdown.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Col, FormGroup, Label } from "reactstrap";
 import { C_Button } from "./CommonButton";
