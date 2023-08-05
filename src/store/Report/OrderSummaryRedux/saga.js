57e1e95896f748a9e07cbd4e953e9194a26a0440
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-08-05 18:22:23 +0530

    Order Summary show and download button changes.

--- src/store/Report/OrderSummaryRedux/saga.js
+++ src/store/Report/OrderSummaryRedux/saga.js
@@ -6,8 +6,10 @@ import { OrderSummaryApiErrorAction, postOrderSummary_API_Success } from "./acti
 import { OderSummary_GoBtn_API } from "../../../helpers/backend_helper";
 
 function* OrderSummary_GenFunc({ config }) {
+    
     try {
         const response = yield call(OderSummary_GoBtn_API, config);
+        response["btnId"] = config.btnId;
         yield put(postOrderSummary_API_Success(response))
     } catch (error) { yield put(OrderSummaryApiErrorAction()) }
 }
