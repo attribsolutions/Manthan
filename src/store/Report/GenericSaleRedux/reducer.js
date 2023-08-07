2ec5f544677f6c7bc8652d90676be44435e57597
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2023-08-07 10:44:42 +0530

    Order Summary and Generic sale Report .
    data not clear after change.

--- src/store/Report/GenericSaleRedux/reducer.js
+++ src/store/Report/GenericSaleRedux/reducer.js
@@ -7,35 +7,16 @@ import {
 
 const INIT_STATE = {
     genericSaleGobtn: [],
-    GoBtnLoading: false,
-    ExcelBtnLoading: false
 }
 
 const GenericSaleReportReducer = (state = INIT_STATE, action) => {
     switch (action.type) {
 
-        case GO_BUTTON_FOR_GENERIC_SALE_ACTION:
-            return {
-                ...state,
-                GoBtnLoading: action.config.btnId,
-                ExcelBtnLoading: action.config.btnId
-            }
-
         case GO_BUTTON_FOR_GENERIC_SALE_SUCCESS:
             return {
                 ...state,
                 genericSaleGobtn: action.payload,
-                GoBtnLoading: false,
-                ExcelBtnLoading: false
             }
-
-        case GENERIC_SALE_REPORT_API_ERROR_ACTION:
-            return {
-                ...state,
-                GoBtnLoading: false,
-                ExcelBtnLoading: false
-            };
-
 
         default:
             return state
--- src/store/Report/OrderSummaryRedux/reducer.js
+++ src/store/Report/OrderSummaryRedux/reducer.js
@@ -1,36 +1,18 @@
-import { ORDER_SUMMARY_API_ERROR_ACTION, POST_ORDER_SUMMARY_API, POST_ORDER_SUMMARY_API_SUCCESS } from "./actionType"
+import { POST_ORDER_SUMMARY_API_SUCCESS } from "./actionType"
 
 const INIT_STATE = {
     orderSummaryGobtn: [],
-    GoBtnLoading: false,
-    ExcelBtnLoading: false
 }
 
 const OrderSummaryReducer = (state = INIT_STATE, action) => {
     switch (action.type) {
 
-        case POST_ORDER_SUMMARY_API:
-            return {
-                ...state,
-                GoBtnLoading: action.config.btnId,
-                ExcelBtnLoading: action.config.btnId
-            }
 
         case POST_ORDER_SUMMARY_API_SUCCESS:
             return {
                 ...state,
                 orderSummaryGobtn: action.payload,
-                GoBtnLoading: false,
-                ExcelBtnLoading: false
             }
-
-        case ORDER_SUMMARY_API_ERROR_ACTION:
-            return {
-                ...state,
-                GoBtnLoading: false,
-                ExcelBtnLoading: false
-            };
-
 
         default:
             return state
