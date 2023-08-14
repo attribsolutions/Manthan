f0ec481157b17ac4b4f0921ac167bb7cc737d5fc
Patu <prathameshshinde179@gmail.com>
2023-08-14 12:07:01 +0530

    Gst R1 report chanes and gst R3B

--- src/store/Report/GSTR1ReportRedux/action.js
+++ src/store/Report/GSTR1ReportRedux/action.js
@@ -2,6 +2,8 @@ import {
     GST_R1_REPORT_API,
     GST_R1_REPORT_API_SUCCESS,
     GST_R1_REPORT_API_ERROR_ACTION,
+    GST_R3B_REPORT_API,
+    GST_R3B_REPORT_API_SUCCESS,
 
 } from "./actionType";
 
@@ -12,6 +14,17 @@ export const GST_R1_Report_API = (config = {}) => ({ // save Action
 
 export const GST_R1_Report_API_Success = (resp) => ({ // Save  success
     type: GST_R1_REPORT_API_SUCCESS,
+    payload: resp,
+});
+
+
+export const GST_R3B_Report_API = (config = {}) => ({ // save Action
+    type: GST_R3B_REPORT_API,
+    config,
+});
+
+export const GST_R3B_Report_API_Success = (resp) => ({ // Save  success
+    type: GST_R3B_REPORT_API_SUCCESS,
     payload: resp,
 });
 
