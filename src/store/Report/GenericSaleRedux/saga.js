7599ee3d432a522556eeaecc4cc84a02998f65e6
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2024-01-09 18:10:18 +0530

    Update saga.js

--- src/store/Report/GenericSaleRedux/saga.js
+++ src/store/Report/GenericSaleRedux/saga.js
@@ -2,7 +2,7 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { GO_BUTTON_FOR_GENERIC_SALE_ACTION } from "./actionType";
 import { GoButton_For_GenericSale_Success } from "./action";
 import { GenericSale_GoBtn_API } from "../../../helpers/backend_helper";
-import { CommonConsole, trailingZeros } from "../../../components/Common/CommonFunction";
+import { CommonConsole, date_dmy_func, trailingZeros } from "../../../components/Common/CommonFunction";
 
 function* GenericSaleReport_GenFunc({ config }) {
 
@@ -10,6 +10,7 @@ function* GenericSaleReport_GenFunc({ config }) {
         const response = yield call(GenericSale_GoBtn_API, config);
 
         const newResponse = response.Data.map((i) => {
+            i["InvoiceDate"]=date_dmy_func(i.InvoiceDate);
             // Convert quantity values to floats and format to remove trailing zeros
             i["QtyInNo"] = trailingZeros(i.QtyInNo);
             i["QtyInKg"] = trailingZeros(i.QtyInKg);
