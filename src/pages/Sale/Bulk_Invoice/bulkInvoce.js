28932c285d12a75916d7a48f8b333ac859c179ba
rohitmainkar <92125293+rohitmainkar@users.noreply.github.com>
2024-01-13 13:40:34 +0530

    Update bulkInvoce.js

--- src/pages/Sale/Bulk_Invoice/bulkInvoce.js
+++ src/pages/Sale/Bulk_Invoice/bulkInvoce.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { useBulkInvoiceContext } from './dataProvider';
 import OrderTable from "./orderTable/index"
+import { balkInvoiceAllOrderAmountFunc } from './util/calculationFunc';
+import { useEffect } from 'react';
+import { BreadcrumbShowCountlabel } from '../../../store/actions';
+import { useDispatch } from 'react-redux';
 
 
 const Invoice = ({ }) => {
-  const { bulkData=[] } = useBulkInvoiceContext()
+
+  const dispatch = useDispatch();
+  const { bulkData = [], globleStockDistribute } = useBulkInvoiceContext();
+
+  useEffect(() => {
+    const { sumOfInvoiceTotal } = balkInvoiceAllOrderAmountFunc({
+      IsTCSParty: false,
+      IsCustomerPAN: false,
+      bulkInvoceInfo: globleStockDistribute
+    });
+    const dataCount = bulkData.length;
+
+    dispatch(BreadcrumbShowCountlabel(`Count:${dataCount} ₹ ${sumOfInvoiceTotal}`))
+
+  }, [globleStockDistribute, bulkData])
+
+
+
   return (
     <div>
       {bulkData.map((order, index) => (
