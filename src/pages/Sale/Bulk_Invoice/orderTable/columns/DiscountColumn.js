57090c1c9f5c6a474b7fe375d0be553c93a237a1
rohitmainkar <rohitganeshaa98@gmail.com>
2024-01-20 10:39:36 +0530

    stash code

---
+++ src/pages/Sale/Bulk_Invoice/orderTable/columns/DiscountColumn.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+import { Input } from 'reactstrap';
+import { CInput, C_Select, decimalRegx } from '../../../../../CustomValidateForm';
+import { useBulkInvoiceContext } from '../../dataProvider';
+import { discountDropOption } from "../../util/constent"
+
+const DiscountColumn = React.memo(({
+  discount,
+  discountType,
+  orderId,
+  itemId,
+  itemAmount,
+}) => {
+  const { handleDiscountChange, handleDiscountTypeChange, } = useBulkInvoiceContext();
+
+  console.log("***********", discount,
+    discountType,
+    orderId,
+    itemId,
+    itemAmount)
+  const handleDiscount = (event) => {
+    const newDiscount = event.target.value
+    handleDiscountChange(orderId, itemId, newDiscount)
+  };
+
+  const handleDiscountType = (newDiscountType) => {
+    handleDiscountTypeChange(orderId, itemId, newDiscountType)
+  };
+
+  //render
+  return (
+    <Fragment>
+      <div className="mb-2">
+        <div>
+          <C_Select
+            value={discountType}
+            options={discountDropOption}
+            onChange={handleDiscountType}
+          />
+        </div>
+
+        <CInput
+          // key={`Dicount_${orderId}-${itemId}-${discount}`}
+          className="right-aligned-placeholder"
+          placeholder="Enter discount value"
+          defaultValue={discount}
+          cpattern={decimalRegx}
+          onChange={handleDiscount}
+        />
+
+      </div>
+      <div className='bg-warning text-dark rounded'>
+        <samp className='text-muted p-2'>Amount:</samp><strong>₹{itemAmount}</strong>
+      </div>
+
+    </Fragment>
+  );
+});
+
+export default DiscountColumn;
