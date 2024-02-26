14997cc27c77dca80ae23cf7a4b781ce29a37dff
Patu <prathameshshinde179@gmail.com>
2024-02-26 12:19:23 +0530

    claim tracking changes

--- src/pages/Accounting/Claim Tracking Entry/ClaimTrackingEntry.js
+++ src/pages/Accounting/Claim Tracking Entry/ClaimTrackingEntry.js
@@ -462,7 +462,8 @@ const ClaimTrackingEntry = (props) => {
         formData.append('Party', values.PartyName.value);
         formData.append('FullClaimNo', values.ClaimText ? values.ClaimText : values.ClaimId.claimId);
         formData.append('PartyType', values.ClaimId.PartyTypeID === undefined ? "" : values.ClaimId.PartyTypeID);
-        if (pageMode === mode.edit) {
+        debugger
+        if (pageMode === mode.edit && values.File[0] === undefined) {
           formData.append(`CreditNoteUpload`, values.CreditNoteUpload);
         } else {
           formData.append(`CreditNoteUpload`, values.File[0]);
