51032f121e2b5aa4467859009f02e3cf39edc469
PunamSonawane135 <105641804+PunamSonawane135@users.noreply.github.com>
2024-03-29 18:02:29 +0530

    Target Vs Achievement : Excel download button add.

--- src/store/Report/TargetVSAchievementRedux/saga.js
+++ src/store/Report/TargetVSAchievementRedux/saga.js
@@ -2,12 +2,22 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { Target_Vs_Achievement_Gobtn_API } from "../../../helpers/backend_helper";
 import { Target_VS_Achievement_Api_ErrorAction, Target_VS_Achievement_Go_Button_API_Success } from "./action";
 import { TARGET_VS_ACHIEVEMENT_GO_BUTTON_API } from "./actionType";
+import { amountCommaSeparateFunc } from "../../../components/Common/CommonFunction";
 
 function* Target_VS_Achievement_GenFun(jsonBody) {
-    debugger
+
     try {
         const response = yield call(Target_Vs_Achievement_Gobtn_API, jsonBody);
-        yield put(Target_VS_Achievement_Go_Button_API_Success(response.Data))
+        const newList = yield response.Data.map((i, k) => {
+            i.id = k + 1
+            i.TargetQuantity = amountCommaSeparateFunc(parseFloat(i.TargetQuantity).toFixed(3))
+            i.TargetAmount = amountCommaSeparateFunc(parseFloat(i.TargetAmount).toFixed(2))
+            i.Quantity = amountCommaSeparateFunc(parseFloat(i.Quantity).toFixed(3))
+            i.Amount = amountCommaSeparateFunc(parseFloat(i.Amount).toFixed(2))
+
+            return i
+        })
+        yield put(Target_VS_Achievement_Go_Button_API_Success(newList))
     } catch (error) { yield put(Target_VS_Achievement_Api_ErrorAction()) }
 }
 
