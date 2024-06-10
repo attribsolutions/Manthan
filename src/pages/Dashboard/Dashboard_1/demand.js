b5c17192fac6f8636b7e0d21a7a62bf54338c5e1
Patu <prathameshshinde179@gmail.com>
2024-06-10 15:38:46 +0530

    Update demand.js

--- src/pages/Dashboard/Dashboard_1/demand.js
+++ src/pages/Dashboard/Dashboard_1/demand.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
 import BootstrapTable from "react-bootstrap-table-next";
 import { date_ymd_func, loginSelectedPartyID } from '../../../components/Common/CommonFunction';
@@ -6,17 +6,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import { globalTableSearchProps } from '../../../components/Common/SearchBox/MySearch';
 import { salesReturnListAPI, salesReturnListAPISuccess } from '../../../store/Sales/SalesReturnRedux/action';
 import SimpleBar from "simplebar-react"
-import { getOrderListPage, getOrderListPageSuccess } from '../../../store/actions';
-import { url } from '../../../routes';
+import { getOrderListPage, getOrderListPageSuccess, makeGRN_Mode_1Action } from '../../../store/actions';
+import { mode, url } from '../../../routes';
+import { printBtnCss } from '../../../components/Common/ListActionsButtons';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Button, Spinner } from 'reactstrap';
 
 
 export default function DemandListForDashboard() {
 
     const dispatch = useDispatch();
+    const history = useHistory();
     const currentDate_ymd = date_ymd_func();
 
-    const { tableList, commonPartyDropSelect } = useSelector((state) => ({
+    const [userAccState, setUserAccState] = useState('');
+
+    const { tableList, commonPartyDropSelect, userAccess, listBtnLoading, GRNitem } = useSelector((state) => ({
         tableList: state.OrderReducer.orderList,
+        GRNitem: state.GRNReducer.GRNitem,
+        userAccess: state.Login.RoleAccessUpdateData,
+        listBtnLoading: state.GRNReducer.listBtnLoading || state.PdfReportReducers.ReportBtnLoading,
         commonPartyDropSelect: state.CommonPartyDropdownReducer.commonPartyDropSelect
     }));
 
@@ -44,6 +53,92 @@ export default function DemandListForDashboard() {
 
     }, [commonPartyDropSelect]);
 
+    useEffect(() => {
+
+        const locationPath = history.location.pathname
+        let userAcc = userAccess.find((inx) => {
+            return (`/${inx.ActualPagePath}` === `/GRN_ADD_3`)
+        })
+
+        if (!(userAcc === undefined)) {
+            setUserAccState(userAcc);
+        }
+    }, [userAccess]);
+
+    useEffect(() => {
+        if (GRNitem.Status === true && GRNitem.StatusCode === 200) {
+            history.push({
+                pathname: GRNitem.path,
+                page_Mode: GRNitem.page_Mode,
+            })
+        }
+    }, [GRNitem])
+
+    function printBtnHandler(rowData, btnId) {
+
+        // let config = {}
+        // config["btnId"] = btnId
+        // config["editId"] = rowData.id
+        // config["ReportType"] = report.invoice;
+        // dispatch(getpdfReportdata(Invoice_Singel_Get_for_Report_Api, config))
+    }
+
+
+    const makeBtnFunc = (list = [], btnId) => {
+        debugger
+        let subPageMode = url.IB_ORDER_SO_LIST
+        const obj = list[0]
+
+        var isGRNSelect = ''
+        var challanNo = ''
+        const grnRef = []
+
+        // list.forEach(ele => {
+
+        grnRef.push({
+            Invoice: null,
+            Order: null,
+            ChallanNo: list.FullOrderNumber,
+            Inward: false,
+            Challan: list.POType === "Challan" ? list.id : '',
+            POType: list.POType,
+            OrderDate: list.OrderDate,
+            CustomerID: list.CustomerID,
+            CustomerName: list.Customer,
+
+        });
+        isGRNSelect = isGRNSelect.concat(`${list.id},`)
+        challanNo = challanNo.concat(`${list.FullOrderNumber},`)
+
+        // });
+
+        if (isGRNSelect) {
+            let path = ""
+            isGRNSelect = isGRNSelect.replace(/,*$/, '');//****** withoutLastComma  function */
+            challanNo = challanNo.replace(/,*$/, '');           //****** withoutLastComma  function */
+
+            // define isMode for MakeBtn API
+
+            if (subPageMode === url.IB_ORDER_SO_LIST) {
+                path = url.CHALLAN
+            }
+
+            const jsonBody = JSON.stringify({
+                OrderIDs: isGRNSelect,
+                DemandDate: subPageMode === url.IB_ORDER_SO_LIST ? list.OrderDate : undefined,
+                Party: subPageMode === url.IB_ORDER_SO_LIST ? loginSelectedPartyID() : undefined
+
+            })
+
+            dispatch(makeGRN_Mode_1Action({ jsonBody, subPageMode, pageMode: mode.defaultsave, path: path, grnRef, challanNo, btnId }))
+
+        } else {
+            alert("Please Select Order1")
+        }
+
+
+    }
+    const hasRole = (role) => userAccState[role];
     const pagesListColumns = [
         {
             text: "Demand Date",
@@ -61,6 +156,55 @@ export default function DemandListForDashboard() {
         {
             text: "Grand Total",
             dataField: "OrderAmount",
+        },
+        {
+            text: "Action",
+            hidden: !hasRole("RoleAccess_IsSave"),
+            dataField: "",
+            formatExtraData: { listBtnLoading: listBtnLoading, },
+            formatter: (cellContent, rowData, key, formatExtra) => {
+
+                let { listBtnLoading } = formatExtra;
+                return (<>
+                    < Button
+                        type="button"
+                        id={`btn-makeBtn-${rowData.id}`}
+                        className="badge badge-soft-info font-size-12 btn btn-info waves-effect waves-light w-xxs border border-light "
+                        title="Make Challan"
+                        onClick={() => {
+                            const btnId = `btn-makeBtn-${rowData.id}`
+                            !listBtnLoading && makeBtnFunc(rowData, btnId)
+                        }}
+                    >
+                        {(listBtnLoading === `btn-makeBtn-${rowData.id}`) ?
+                            <Spinner style={{ height: "16px", width: "16px" }} color="white" />
+                            : <span
+                                className=" fas fa-file-invoice font-size-17"
+                            ></span>
+                        }
+                    </Button>
+
+                    < Button
+                        type="button"
+                        id={`btn-print-${rowData.id}`}
+                        className={printBtnCss}
+                        style={{ marginLeft: "9px" }}
+                        title="Print Challan"
+                        onClick={() => {
+                            const btnId = `btn-print-${rowData.id}`
+                            !listBtnLoading && printBtnHandler(rowData, btnId)
+                        }}
+                    >
+                        {(listBtnLoading === `btn-print-${rowData.id}`) ?
+                            <Spinner style={{ height: "16px", width: "16px" }} color="white" />
+                            : <span
+
+                                className="bx bx-printer font-size-16"
+                            ></span>
+                        }
+                    </Button>
+                </>)
+            }
         },
 
     ];
