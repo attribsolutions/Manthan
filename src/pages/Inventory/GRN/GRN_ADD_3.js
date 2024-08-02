fff9efa0788cd4deab1548408d8dae11ed03d092
Patu <prathameshshinde179@gmail.com>
2024-08-02 16:28:38 +0530

    Update GRN_ADD_3.js

--- src/pages/Inventory/GRN/GRN_ADD_3.js
+++ src/pages/Inventory/GRN/GRN_ADD_3.js
@@ -395,6 +395,7 @@ const GRNAdd3 = (props) => {
     const saveHandeller = (event) => {
         event.preventDefault();
         const btnId = event.target.id
+        
         try {
             const isvalidMsg = [];
 
@@ -434,6 +435,7 @@ const GRNAdd3 = (props) => {
                 GrandTotal: grnDetail.OrderAmount,
                 Party: grnDetail.Supplier,
                 InvoiceNumber: invoiceNo,
+                InvoiceDate: _cfunc.date_ymd_func(invoiceDate),
                 CreatedBy: _cfunc.loginUserID(),
                 UpdatedBy: 1,
                 GRNItems: itemArray,
