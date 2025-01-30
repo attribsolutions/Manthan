437412a78c48867f3c437bb8292b39b517229096
Swapnil <swapnilkumbhar.1101@gmail.com>
2025-01-30 16:09:02 +0530

    api integrated in GRN_ADD page

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -350,6 +350,7 @@ export const ORDER_PRINT_ALL_API = '/OrdersPrint'
 
 export const GRN_STP_FOR_ORDER_lIST_goBtn = "/OrdersFilterSecond"
 export const GRN_MAKE_API = "/MakeOrdersGrn"
+export const INVOICE_NO_MESSAGE = "/GRNSaveforCSS"
 export const GRN = "/GRN"
 export const GRN_LiST_BY_FILTERS = "/GRNFilter"
 export const HIDE_INVOICE_FOR_GRN = "/InvoiceHide"
