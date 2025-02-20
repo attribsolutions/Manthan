4571538f8e3a94819a957c74a0db6f29081d31c3
Patu <prathameshshinde179@gmail.com>
2025-02-20 18:35:45 +0530

    changes

--- src/store/Report/CodeRedemptionRedux/saga.js
+++ src/store/Report/CodeRedemptionRedux/saga.js
@@ -2,6 +2,7 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { CODE_REDEMPTION_REPORT_ACTION } from "./actionType";
 import { CodeRedemption_Report_Action_Success, CodeRedemption_Report_ErrorAction } from "./action";
 import { CodeRedemptionReport_API, } from "../../../helpers/backend_helper";
+import { date_dmy_func } from "../../../components/Common/CommonFunction";
 
 
 function* CodeRedemptionReport_GenFunc({ config }) {
@@ -9,6 +10,11 @@ function* CodeRedemptionReport_GenFunc({ config }) {
 	try {
 		let response = yield call(CodeRedemptionReport_API, config);
 		response["Mode"] = config.Mode
+		response.Data = response.Data.map(item => ({
+			...item,
+			InvoiceDate: date_dmy_func(item.InvoiceDate),
+			recordsAmountTotal: item.InvoiceAmount  // You can set the flag value as needed
+		}));
 		yield put(CodeRedemption_Report_Action_Success(response))
 	} catch (error) { yield put(CodeRedemption_Report_ErrorAction()) }
 }
