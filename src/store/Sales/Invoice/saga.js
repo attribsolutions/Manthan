07b55b9a2678c568d2211cbd64299c2474feb726
Patu <prathameshshinde179@gmail.com>
2025-04-02 11:28:57 +0530

    Update saga.js

--- src/store/Sales/Invoice/saga.js
+++ src/store/Sales/Invoice/saga.js
@@ -148,7 +148,7 @@ function* InvoiceListGenFunc({ config }) {
       debugger
       if (isSweetAndSnacksCompany && !(subPageMode === url.IB_INVOICE_FOR_GRN)) {
         if (i.IsSendToFTPSAP) {
-          i["isSend"] = true
+          i["isSend"] = false
         } else {
           i["isSend"] = true
         }
