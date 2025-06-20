c6167c71d01700966aff9e4a40c824495a1cf5ac
Patu <prathameshshinde179@gmail.com>
2025-06-20 11:05:07 +0530

    chanegs

--- src/store/Report/ItemConsumptionReportRedux/saga.js
+++ src/store/Report/ItemConsumptionReportRedux/saga.js
@@ -7,14 +7,9 @@ import { date_dmy_func } from "../../../components/Common/CommonFunction";
 
 function* ItemConsumptionReport_GenFunc({ config }) {
 	try {
-
+		debugger
 		let response = yield call(ItemConsumption_API, config);
-		const newList = yield response.Data.map((i) => {
-			i["recordsAmountTotal"] = i.Amount;  // Breadcrumb Count total
-			i.InvoiceDate = date_dmy_func(i.InvoiceDate); // Only for Dashoard 
-			return i
-		})
-		response["Data"] = newList
+
 		response["goBtnMode"] = config.goBtnMode
 		yield put(ItemConsumptionReport_GoButton_API_Success(response))
 	} catch (error) { yield put(ItemConsumptionApiErrorAction()) }
