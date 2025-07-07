adeea96522572f670e88dc0cd69d7b6970c25b72
Patu <prathameshshinde179@gmail.com>
2025-07-07 12:52:41 +0530

    chanegs stock adjust ment report

--- src/store/Report/StockAdjustMentRedux/saga.js
+++ src/store/Report/StockAdjustMentRedux/saga.js
@@ -2,12 +2,25 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { STOCK_ADJUSTMENT_REPORT_ACTION, } from "./actionType";
 import { Stock_adjustment_Report_ApiErrorAction, Stock_adjustment_Report_Success } from "./action";
 import { Stock_Adjustment_Report_GoButton_API, } from "../../../helpers/backend_helper"
+import { date_dmy_func } from "../../../components/Common/CommonFunction";
 
 function* Stock_Adjustment_Report_GenFunc({ config }) {
     try {
         let response = yield call(Stock_Adjustment_Report_GoButton_API, config);
         debugger
         response.Type = config.btnId
+
+        response.Data = response.Data.map((i, index) => {
+            return {
+                ...i,
+                id: index + 1,
+                StockDate: date_dmy_func(i.StockDate),
+                Quantity: i.Quantity.toFixed(2),
+                Difference: i.Difference.toFixed(2),
+                BeforeAdjustment: i.BeforeAdjustment.toFixed(2),
+            };
+        });
+
         yield put(Stock_adjustment_Report_Success(response))
     } catch (error) { yield put(Stock_adjustment_Report_ApiErrorAction()) }
 }
